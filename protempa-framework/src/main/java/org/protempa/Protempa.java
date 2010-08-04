6fbf05afd0e8b3be3f01e0099910b406ab4384a1
Himanshu Rathod <himanshu.rathod@emory.edu>
2010-08-04 20:50:13 +0000

    Removed extraneous imports.  Moved the asSet method to the Arrays class in the Javautils module.  Added method signatures for future work on query sessions.

--- protempa-framework/src/main/java/org/protempa/Protempa.java
+++ protempa-framework/src/main/java/org/protempa/Protempa.java
@@ -1,28 +1,23 @@
 package org.protempa;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
+import org.arp.javautil.arrays.Arrays;
 import org.protempa.backend.BackendNewInstanceException;
 import org.protempa.backend.BackendProviderSpecLoaderException;
 import org.protempa.backend.ConfigurationsLoadException;
 import org.protempa.backend.InvalidConfigurationsException;
 import org.protempa.dsb.datasourceconstraint.DataSourceConstraint;
-import org.protempa.proposition.Proposition;
 import org.protempa.query.Query;
 import org.protempa.query.handler.QueryResultsHandler;
-
 
 /**
  * Main object for users of PROTEMPA.
  * 
  * TODO Finders should return an object that implements a Results interface.
  * Objects could store results in-memory or in a database (e.g., an interval
- * database). Ideally, for stateful support, intervals in such a store could
- * be reloaded into working memory. This would be a kind of star schema.
+ * database). Ideally, for stateful support, intervals in such a store could be
+ * reloaded into working memory. This would be a kind of star schema.
  * 
  * @author Andrew Post
  */
@@ -44,8 +39,7 @@ public final class Protempa {
                 sourceFactory.newAlgorithmSourceInstance(), false);
     }
 
-    public static Protempa newInstance(String configurationsId,
-            boolean useCache)
+    public static Protempa newInstance(String configurationsId, boolean useCache)
             throws ConfigurationsLoadException, BackendInitializationException,
             BackendNewInstanceException, BackendProviderSpecLoaderException,
             InvalidConfigurationsException {
@@ -53,8 +47,8 @@ public final class Protempa {
     }
 
     public static Protempa newInstance(SourceFactory sourceFactory,
-            boolean useCache)
-            throws BackendInitializationException, BackendNewInstanceException {
+            boolean useCache) throws BackendInitializationException,
+            BackendNewInstanceException {
         return new Protempa(sourceFactory.newDataSourceInstance(),
                 sourceFactory.newKnowledgeSourceInstance(),
                 sourceFactory.newAlgorithmSourceInstance(), useCache);
@@ -63,10 +57,10 @@ public final class Protempa {
     /**
      * Constructor that configures PROTEMPA not to cache found abstract
      * parameters.
-     *
+     * 
      * @param dataSource
      *            a <code>DataSource</code>.
-     *
+     * 
      * @param knowledgeSource
      *            a <code>KnowledgeSource</code>.
      * @param algorithmSource
@@ -80,10 +74,10 @@ public final class Protempa {
     /**
      * Constructor that lets the user specify whether or not to cache found
      * abstract parameters.
-     *
+     * 
      * @param dataSource
      *            a <code>DataSource</code>.
-     *
+     * 
      * @param knowledgeSource
      *            a <code>KnowledgeSource</code>.
      * @param algorithmSource
@@ -99,48 +93,62 @@ public final class Protempa {
             throw new IllegalArgumentException("dataSource cannot be null");
         }
         if (knowledgeSource == null) {
-            throw new IllegalArgumentException(
-                    "knowledgeSource cannot be null");
+            throw new IllegalArgumentException("knowledgeSource cannot be null");
         }
         if (algorithmSource == null) {
-            throw new IllegalArgumentException(
-                    "algorithmSource cannot be null");
+            throw new IllegalArgumentException("algorithmSource cannot be null");
         }
         this.abstractionFinder = new AbstractionFinder(dataSource,
-                knowledgeSource, algorithmSource,
-                cacheFoundAbstractParameters);
+                knowledgeSource, algorithmSource, cacheFoundAbstractParameters);
     }
 
-    public void execute(Query query, QueryResultsHandler resultHandler) throws FinderException {
+    public void detachSession(QuerySession querySession) {
+        // TODO: implement me.
+    }
+
+    public void reattachSession(QuerySession querySession) {
+        // TODO: implement me
+    }
+
+    public void deleteSession(QuerySession querySession) {
+        // TODO: implement me
+    }
+
+    /**
+     * Create a QuerySession object, which can be used by the caller to further
+     * filter or drill the results of the initial query that is passed in.
+     * 
+     * @param query
+     *            The query object to set up in the database for subsequent
+     *            queries
+     * @return A QuerySession object, containing initial query state
+     */
+    public QuerySession prepare(Query query) {
+        // TODO: implement me
+        if (query == null) {
+            throw new IllegalArgumentException("query cannot be null");
+        }
+        return new QuerySession(query, this.abstractionFinder);
+    }
+
+    public void execute(Query query, QueryResultsHandler resultHandler)
+            throws FinderException {
         if (query == null)
             throw new IllegalArgumentException("query cannot be null");
 
-//        Map<String, List<Proposition>> results =
-//                new HashMap<String, List<Proposition>>();
+        // Map<String, List<Proposition>> results =
+        // new HashMap<String, List<Proposition>>();
 
-        Set<String> keyIdsSet = asSet(query.getKeyIds());
-        Set<String> propIds = asSet(query.getPropIds());
-        DataSourceConstraint dataSourceConstraints =
-                    query.getDataSourceConstraints();
-        
-//        results.putAll()
-        		this.abstractionFinder.doFind(keyIdsSet,
-                propIds, dataSourceConstraints, resultHandler);
-        
-//        return new QueryResults(results);
-    }
+        Set<String> keyIdsSet = Arrays.asSet(query.getKeyIds());
+        Set<String> propIds = Arrays.asSet(query.getPropIds());
+        DataSourceConstraint dataSourceConstraints = query
+                .getDataSourceConstraints();
 
-    private Set<String> asSet(String[] propIds) {
-        Set<String> propIdsSet = new HashSet<String>();
-        if (propIds != null) {
-            propIdsSet = new HashSet<String>();
-            for (String propId : propIds) {
-                propIdsSet.add(propId);
-            }
-        } else {
-            propIdsSet = null;
-        }
-        return propIdsSet;
+        // results.putAll()
+        this.abstractionFinder.doFind(keyIdsSet, propIds,
+                dataSourceConstraints, resultHandler);
+
+        // return new QueryResults(results);
     }
 
     /**
