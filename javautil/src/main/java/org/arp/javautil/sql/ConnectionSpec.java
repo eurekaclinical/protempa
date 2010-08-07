22e681a3fed5e27a2de7f58a6110031015322c1b
Andrew Post <arpost@emory.edu>
2010-08-07 21:43:47 +0000

    Created an API that abstracts away Java's two database access APIs.

--- javautil/src/main/java/org/arp/javautil/sql/ConnectionSpec.java
+++ javautil/src/main/java/org/arp/javautil/sql/ConnectionSpec.java
@@ -1,10 +1,20 @@
 package org.arp.javautil.sql;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.SQLException;
 
+/**
+ * Provides an interface for getting a connection to a relational database.
+ *
+ * @author Andrew Post
+ */
 public interface ConnectionSpec {
+    /**
+     * Returns a connection to a SQL database that may be new or part of a
+     * connection pool.
+     *
+     * @return a {@link Connection}.
+     * @throws SQLException if an error occurs getting the connection.
+     */
     Connection getOrCreate() throws SQLException;
-    Driver getDriver() throws SQLException;
 }
--- javautil/src/main/java/org/arp/javautil/sql/DriverManagerConnectionSpec.java
+++ javautil/src/main/java/org/arp/javautil/sql/DriverManagerConnectionSpec.java
@@ -1,12 +1,13 @@
 package org.arp.javautil.sql;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- *
+ * Implements getting a connection to a relational database using Java's
+ * {@link DriverManager} API.
+ * 
  * @author Andrew Post
  */
 public class DriverManagerConnectionSpec implements ConnectionSpec {
@@ -14,17 +15,32 @@ public class DriverManagerConnectionSpec implements ConnectionSpec {
     private String user;
     private String password;
 
-    public DriverManagerConnectionSpec(String url, String user,
-            String password) {
+    /**
+     * Creates an instance with a specified JDBC URL, and a username and
+     * password.
+     * 
+     * @param url a JDBC URL {@link String}.
+     * @param user a username {@link String}.
+     * @param password a password {@link String}.
+     */
+    public DriverManagerConnectionSpec(String url, String user, String password)
+    {
         this.url = url;
         this.user = user;
         this.password = password;
     }
 
-    public Driver getDriver() throws SQLException {
-        return DriverManager.getDriver(this.url);
-    }
-
+    /**
+     * Creates a database connection or gets an existing connection with
+     * the JDBC URL, username and password specified in the constructor.
+     *
+     * @return a {@link Connection}.
+     *
+     * @throws SQLException if an error occurred creating/getting a
+     * {@link Connection}, possibly because the JDBC URL, username and/or
+     * password are invalid.
+     */
+    @Override
     public Connection getOrCreate() throws SQLException {
         return DriverManager.getConnection(this.url, this.user, this.password);
     }
