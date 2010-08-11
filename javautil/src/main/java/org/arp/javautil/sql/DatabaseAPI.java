a2d96b51f34e72e3ce765c56c529136eeedfe7e8
Andrew Post <arpost@emory.edu>
2010-08-11 02:06:43 +0000

    Added documentation for DatabaseAPI.

--- javautil/src/main/java/org/arp/javautil/sql/DatabaseAPI.java
+++ javautil/src/main/java/org/arp/javautil/sql/DatabaseAPI.java
@@ -3,14 +3,54 @@ package org.arp.javautil.sql;
 import javax.naming.NamingException;
 
 /**
- * For specifying which Java database API to use for getting database
- * connections ({@link java.sql.DriverManager} or
- * {@link javax.sql.DataSource}).
+ * An enum of Java's APIs (({@link java.sql.DriverManager} and
+ * {@link javax.sql.DataSource}) for connecting to relational databases. It
+ * provides a straightforward means for programs to make use of either
+ * without having to hard-code the API choice. This is useful in developing a
+ * library that may be used in a standalone program or in a container.
+ *
+ * Usage:
+ *
+ * <code>
+ * public class DatabaseAPIExample {
+ *     private DatabaseAPI databaseAPI;
+ *     private String databaseId;
+ *     private String username;
+ *     private String password;
+ *
+ *     public DatabaseAPIExample(DatabaseAPI databaseAPI, String databaseId,
+ *         String username, String password) {
+ *         this.databaseAPI = databasePID;
+ *         this.databaseId = databaseId;
+ *         this.username = username;
+ *         this.password = password;
+ *     }
+ *
+ *     public void doProcessData() {
+ *         ConnectionSpec connectionSpec =
+ *                 this.databaseAPI.newConnectionSpecInstance(
+ *                     this.databaseId, this.username, this.password);
+ *         ...
+ *     }
+ * }
+ * </code>
  */
 public enum DatabaseAPI {
 
     DRIVERMANAGER, DATASOURCE;
 
+    /**
+     * Gets a {@link ConnectionSpec} implementation that is specific to the
+     * selected database API for getting/creating a database {@link Connection}.
+     * 
+     * @param databaseId the identifier for the database (JDBC URL for
+     * the {@link java.sql.DriverManager} API, JNDI name for the
+     * {@link javax.sql.DataSource} API.
+     * @param username the username (<code>null</code> if not applicable)
+     * @param password the password (<code>null</code> if not applicable)
+     * @return a {@link ConnectionSpec}.
+     * @throws InvalidConnectionSpecArguments if the databaseId is invalid.
+     */
     public ConnectionSpec newConnectionSpecInstance(String databaseId, 
             String username, String password)
             throws InvalidConnectionSpecArguments {
