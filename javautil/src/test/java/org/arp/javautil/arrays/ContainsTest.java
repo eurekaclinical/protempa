2c4f03da19faf40feef88685ad710c2ea51d5131
Andrew Post <arpost@emory.edu>
2010-08-12 23:15:55 +0000

    Removed unneeded overriding methods (they weren't doing anything).

--- javautil/src/test/java/org/arp/javautil/arrays/ContainsTest.java
+++ javautil/src/test/java/org/arp/javautil/arrays/ContainsTest.java
@@ -1,6 +1,5 @@
 package org.arp.javautil.arrays;
 
-import org.arp.javautil.arrays.Arrays;
 
 import junit.framework.TestCase;
 
@@ -10,26 +9,6 @@ import junit.framework.TestCase;
  * @author Andrew Post
  */
 public class ContainsTest extends TestCase {
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
 
 	public void testContainsObjectTrue() {
 		assertTrue(Arrays.contains(new String[] { "hello", "world", "!" },
