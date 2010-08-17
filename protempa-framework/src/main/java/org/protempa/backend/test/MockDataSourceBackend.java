6fca05ef19dc343ea34ac0541f91791fcba13549
Himanshu Rathod <himanshu.rathod@emory.edu>
2010-08-17 13:38:28 +0000

    Added the QuerySession parameter to all get*() and read*() methods.  This change is in preparation for a temporary table creation mechanism.

--- protempa-framework/src/main/java/org/protempa/backend/test/MockDataSourceBackend.java
+++ protempa-framework/src/main/java/org/protempa/backend/test/MockDataSourceBackend.java
@@ -21,78 +21,97 @@ import org.protempa.proposition.value.UnitFactory;
 public final class MockDataSourceBackend implements DataSourceBackend {
 
     public List<String> getAllKeyIds(int start, int finish,
-            DataSourceConstraint dataSourceConstraints)
+            DataSourceConstraint dataSourceConstraints, QuerySession qs)
             throws DataSourceReadException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Map<String, List<ConstantParameter>> getConstantParameters(
             Set<String> keyIds,
-            Set<String> paramIds, DataSourceConstraint dataSourceConstraints)
+            Set<String> paramIds, DataSourceConstraint dataSourceConstraints,
+            QuerySession qs)
             throws DataSourceReadException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Map<String, List<Event>> getEventsAsc(Set<String> keyIds, 
-            Set<String> eventIds, DataSourceConstraint dataSourceConstraints)
+            Set<String> eventIds, DataSourceConstraint dataSourceConstraints,
+            QuerySession qs)
             throws DataSourceReadException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Map<String, List<Event>> getEventsDesc(Set<String> keyIds,
-            Set<String> eventIds, DataSourceConstraint dataSourceConstraints)
+            Set<String> eventIds, DataSourceConstraint dataSourceConstraints,
+            QuerySession qs)
             throws DataSourceReadException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public GranularityFactory getGranularityFactory() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Map<String, List<PrimitiveParameter>> getPrimitiveParametersAsc(
             Set<String> keyIds, Set<String> paramIds,
-            DataSourceConstraint dataSourceConstraints)
+            DataSourceConstraint dataSourceConstraints,
+            QuerySession qs)
             throws DataSourceReadException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Map<String, List<PrimitiveParameter>> getPrimitiveParametersDesc(
             Set<String> keyIds, Set<String> paramIds,
-            DataSourceConstraint dataSourceConstraints)
+            DataSourceConstraint dataSourceConstraints,
+            QuerySession qs)
             throws DataSourceReadException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public UnitFactory getUnitFactory() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public String getKeyType() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public String getKeyTypeDisplayName() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public String getKeyTypePluralDisplayName() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void initialize(BackendInstanceSpec config)
             throws BackendInitializationException {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void close() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void addBackendListener(
             BackendListener<DataSourceBackendUpdatedEvent> listener) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void removeBackendListener(
             BackendListener<DataSourceBackendUpdatedEvent> listener) {
         throw new UnsupportedOperationException("Not supported yet.");
