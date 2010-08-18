824f8426f8604e507f03768312baff37fe66cb78
Andrew Post <arpost@emory.edu>
2010-08-18 21:24:14 +0000

    Added argument validation.

--- javautil/src/main/java/org/arp/javautil/sql/SQLExecutor.java
+++ javautil/src/main/java/org/arp/javautil/sql/SQLExecutor.java
@@ -12,93 +12,108 @@ import java.sql.Statement;
  * @author Andrew Post
  */
 public final class SQLExecutor {
-	public static interface StatementPreparer {
-		void prepare(PreparedStatement stmt) throws SQLException;
-	}
 
-	public static interface ResultProcessor {
-		void process(ResultSet resultSet) throws SQLException;
-	}
+    public static interface StatementPreparer {
 
-	public static void executeSQL(Connection connection,
-			PreparedStatement preparedStmt, StatementPreparer stmtPreparer,
-			ResultProcessor resultProcessor) throws SQLException {
+        void prepare(PreparedStatement stmt) throws SQLException;
+    }
 
-		if (stmtPreparer != null) {
-			stmtPreparer.prepare(preparedStmt);
-		}
+    public static interface ResultProcessor {
 
-		ResultSet resultSet = null;
-		try {
-			preparedStmt.execute();
-			if (resultProcessor != null) {
-				resultProcessor.process(preparedStmt.getResultSet());
-			}
-		} finally {
-			if (resultSet != null) {
-				resultSet.close();
-			}
-		}
-	}
+        void process(ResultSet resultSet) throws SQLException;
+    }
 
-	public static void executeSQL(Connection connection, String sql,
-			ResultProcessor resultProcessor) throws SQLException {
-            Statement stmt = connection.createStatement();
+    public static void executeSQL(Connection connection,
+            PreparedStatement preparedStmt, StatementPreparer stmtPreparer,
+            ResultProcessor resultProcessor) throws SQLException {
+        if (connection == null)
+            throw new IllegalArgumentException("connection cannot be null");
+        if (stmtPreparer != null) {
+            stmtPreparer.prepare(preparedStmt);
+        }
+
+        ResultSet resultSet = null;
+        try {
+            preparedStmt.execute();
+            if (resultProcessor != null) {
+                resultProcessor.process(preparedStmt.getResultSet());
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+    }
+
+    public static void executeSQL(Connection connection, String sql,
+            ResultProcessor resultProcessor) throws SQLException {
+        if (connection == null)
+            throw new IllegalArgumentException("connection cannot be null");
+        Statement stmt = connection.createStatement();
+        try {
+            ResultSet resultSet = null;
             try {
-                ResultSet resultSet = null;
-                try {
-                    stmt.execute(sql);
-                    if (resultProcessor != null) {
-                            resultProcessor.process(stmt.getResultSet());
-                    }
-                } finally {
-                    if (resultSet != null) {
-                        resultSet.close();
-                    }
+                stmt.execute(sql);
+                if (resultProcessor != null) {
+                    resultProcessor.process(stmt.getResultSet());
                 }
             } finally {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            }
+        } finally {
+            stmt.close();
+        }
+    }
+
+    public static void executeSQL(Connection connection, String sql,
+            StatementPreparer stmtPreparer, ResultProcessor resultProcessor)
+            throws SQLException {
+        if (connection == null)
+            throw new IllegalArgumentException("connection cannot be null");
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement(sql);
+            executeSQL(connection, stmt, stmtPreparer, resultProcessor);
+        } finally {
+            if (stmt != null) {
                 stmt.close();
             }
-	}
+        }
+    }
 
-	public static void executeSQL(Connection connection, String sql,
-			StatementPreparer stmtPreparer, ResultProcessor resultProcessor)
-			throws SQLException {
-		PreparedStatement stmt = null;
-		try {
-			stmt = connection.prepareStatement(sql);
-			executeSQL(connection, stmt, stmtPreparer, resultProcessor);
-		} finally {
-			if (stmt != null) {
-				stmt.close();
-			}
-		}
-	}
+    public static void executeSQL(ConnectionSpec connectionCreator,
+            String sql, ResultProcessor resultProcessor)
+            throws SQLException {
+        if (connectionCreator == null)
+            throw new IllegalArgumentException(
+                    "connectionCreator cannot be null");
+        Connection con = null;
+        try {
+            con = connectionCreator.getOrCreate();
+            executeSQL(con, sql, resultProcessor);
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
 
-	public static void executeSQL(ConnectionSpec connectionCreator,
-			String sql, ResultProcessor resultProcessor) throws SQLException {
-		Connection con = null;
-		try {
-			con = connectionCreator.getOrCreate();
-			executeSQL(con, sql, resultProcessor);
-		} finally {
-			if (con != null) {
-				con.close();
-			}
-		}
-	}
-
-	public static void executeSQL(ConnectionSpec connectionCreator,
-			String sql, StatementPreparer stmtPreparer,
-			ResultProcessor resultProcessor) throws SQLException {
-		Connection con = null;
-		try {
-			con = connectionCreator.getOrCreate();
-			executeSQL(con, sql, stmtPreparer, resultProcessor);
-		} finally {
-			if (con != null) {
-				con.close();
-			}
-		}
-	}
+    public static void executeSQL(ConnectionSpec connectionCreator,
+            String sql, StatementPreparer stmtPreparer,
+            ResultProcessor resultProcessor) throws SQLException {
+        if (connectionCreator == null)
+            throw new IllegalArgumentException(
+                    "connectionCreator cannot be null");
+        Connection con = null;
+        try {
+            con = connectionCreator.getOrCreate();
+            executeSQL(con, sql, stmtPreparer, resultProcessor);
+        } finally {
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
 }
