9ff0f7b647f4046b50654eca791745c1193e6b91
Andrew Post <arpost@emory.edu>
2010-08-22 14:37:52 +0000

    Fixed compile errors. There's no way to test whether I introduced bugs.

--- protempa-ksb-protege/src/test/java/org/protempa/ksb/protege/AbstractHELLPAllKeysOneParameterTest.java
+++ protempa-ksb-protege/src/test/java/org/protempa/ksb/protege/AbstractHELLPAllKeysOneParameterTest.java
@@ -1,11 +1,12 @@
 package org.protempa.ksb.protege;
 
 import java.util.List;
+import java.util.Map;
 
-import org.protempa.DataSourceReadException;
 import org.protempa.FinderException;
 import org.protempa.query.SimpleQuery;
 import org.protempa.proposition.Proposition;
+import org.protempa.query.handler.MappingQueryResultsHandler;
 
 /**
  * This is a test to verify if the number of cases for a parameter or a
@@ -14,62 +15,46 @@ import org.protempa.proposition.Proposition;
  * @author Nora Sovarel
  * 
  */
-public abstract class AbstractHELLPAllKeysOneParameterTest 
+public abstract class AbstractHELLPAllKeysOneParameterTest
         extends AbstractTest {
 
-	protected class TestProtempaRunner {
+    protected class TestProtempaRunner {
 
-		private String propId;
+        private String propId;
+        private int numberOfIntervalsFound;
+        private int numberOfKeysFound;
 
-		private int numberOfIntervalsFound;
-		
-		private int numberOfKeysFound;
+        TestProtempaRunner(String propId) {
+            this.propId = propId;
+        }
 
-		TestProtempaRunner(String propId) {
-			this.propId = propId;
-		}
+        public void run() throws FinderException {
+            this.numberOfIntervalsFound = 0;
+            this.numberOfKeysFound = 0;
 
-		public void run() throws FinderException {
-			this.numberOfIntervalsFound = 0;
-			this.numberOfKeysFound = 0;
-			
-			try {
-                int i = 0;
-                while (true) {
-                    List<String> keyIds =
-                        AbstractHELLPAllKeysOneParameterTest.dataSource
-                        .getAllKeyIds(i,1000);
-                    if (keyIds.isEmpty()) {
-                        break;
-                    }
-                    for (String keyId : keyIds) {
-                        List<Proposition> results = protempa.execute(
-                                SimpleQuery.newInstance(keyId,propId))
-                                .get(keyId);
-                        if (!results.isEmpty()) {
-                            numberOfIntervalsFound += results.size();
-                            numberOfKeysFound++;
-                        }
-                    }
-                    i += 1000;
+            try {
+                MappingQueryResultsHandler mqrh =
+                        new MappingQueryResultsHandler();
+                protempa.execute(SimpleQuery.newFindForAllKeysInstance(propId),
+                        mqrh);
+                Map<String, List<Proposition>> results = mqrh.getResultMap();
+                this.numberOfKeysFound = results.size();
+                for (List<Proposition> vals : results.values()) {
+                    this.numberOfIntervalsFound += vals.size();
                 }
-            } catch (DataSourceReadException ex) {
-                throw new FinderException(ex);
-			} catch (FinderException fe) {
-				throw fe;
-			} catch (Exception e) {
-				throw new FinderException("An error occurred", e);
-			}
-		}
+            } catch (FinderException fe) {
+                throw fe;
+            } catch (Exception e) {
+                throw new FinderException("An error occurred", e);
+            }
+        }
 
-		int getNumberOfIntervalsFound() {
-			return this.numberOfIntervalsFound;
-		}
-		
-		int getNumberOfKeysFound() {
-			return this.numberOfKeysFound;
-		}
+        int getNumberOfIntervalsFound() {
+            return this.numberOfIntervalsFound;
+        }
 
-	}
-
+        int getNumberOfKeysFound() {
+            return this.numberOfKeysFound;
+        }
+    }
 }
--- protempa-ksb-protege/src/test/java/org/protempa/ksb/protege/HELLPDataSourceBackend.java
+++ protempa-ksb-protege/src/test/java/org/protempa/ksb/protege/HELLPDataSourceBackend.java
@@ -1,28 +1,62 @@
 package org.protempa.ksb.protege;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.arp.javautil.sql.ConnectionSpec;
+import org.arp.javautil.sql.DatabaseAPI;
+import org.arp.javautil.sql.InvalidConnectionSpecArguments;
+import org.arp.javautil.sql.SQLExecutor;
+import org.arp.javautil.sql.SQLExecutor.ResultProcessor;
+import org.arp.javautil.sql.SQLExecutor.StatementPreparer;
+import org.protempa.DataSourceBackendInitializationException;
+import org.protempa.DataSourceReadException;
+import org.protempa.QuerySession;
+import org.protempa.backend.BackendInstanceSpec;
 import org.protempa.bp.commons.AbstractCommonsDataSourceBackend;
 import org.protempa.bp.commons.BackendInfo;
 import org.protempa.bp.commons.BackendProperty;
+import org.protempa.dsb.filter.Filter;
+import org.protempa.proposition.ConstantParameter;
+import org.protempa.proposition.Event;
+import org.protempa.proposition.PointInterval;
+import org.protempa.proposition.PrimitiveParameter;
+import org.protempa.proposition.value.GranularityFactory;
+import org.protempa.proposition.value.RelativeHourGranularity;
+import org.protempa.proposition.value.RelativeHourGranularityFactory;
+import org.protempa.proposition.value.RelativeHourUnitFactory;
+import org.protempa.proposition.value.UnitFactory;
+import org.protempa.proposition.value.ValueFormat;
+import org.protempa.proposition.value.ValueType;
 
 /**
  *
  * @author Andrew Post
  */
-@BackendInfo (
-    displayName="HELLP Data"
-)
+@BackendInfo(displayName = "HELLP Data")
 public class HELLPDataSourceBackend extends AbstractCommonsDataSourceBackend {
-    
-    private String driverClass;
 
     private String dbUrl;
-
     private String username;
-    
     private String password;
+    private String driverClass;
+    private final RelativeHourGranularityFactory granularityFactory;
+    private final RelativeHourUnitFactory unitFactory;
+    private ConnectionSpec connectionSpecInstance;
 
-    public String getDbUrl() {
-        return dbUrl;
+    /**
+     *
+     */
+    public HELLPDataSourceBackend() {
+        this.granularityFactory = new RelativeHourGranularityFactory();
+        this.unitFactory = new RelativeHourUnitFactory();
     }
 
     @BackendProperty
@@ -30,26 +64,8 @@ public class HELLPDataSourceBackend extends AbstractCommonsDataSourceBackend {
         this.dbUrl = dbUrl;
     }
 
-    public String getDriverClass() {
-        return driverClass;
-    }
-
-    @BackendProperty
-    public void setDriverClass(String driverClass) {
-        this.driverClass = driverClass;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @BackendProperty
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
+    public String getDbUrl() {
+        return this.dbUrl;
     }
 
     @BackendProperty
@@ -57,8 +73,299 @@ public class HELLPDataSourceBackend extends AbstractCommonsDataSourceBackend {
         this.username = username;
     }
 
-    public HELLPDataSourceBackend() {
-        super(new HELLPSchemaAdaptor());
+    public String getUsername() {
+        return this.username;
     }
 
+    @BackendProperty
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    @BackendProperty
+    public void setDriverClass(String driverClass) {
+        this.driverClass = driverClass;
+    }
+
+    public String getDriverClass() {
+        return this.driverClass;
+    }
+
+    @Override
+    public GranularityFactory getGranularityFactory() {
+        return this.granularityFactory;
+    }
+
+    @Override
+    public UnitFactory getUnitFactory() {
+        return this.unitFactory;
+    }
+
+    @Override
+    public void initialize(BackendInstanceSpec config)
+            throws DataSourceBackendInitializationException {
+        super.initialize(config);
+        try {
+            connectionSpecInstance =
+                    DatabaseAPI.DRIVERMANAGER.newConnectionSpecInstance(
+                    this.dbUrl, this.username, this.password);
+        } catch (InvalidConnectionSpecArguments ex) {
+            throw new DataSourceBackendInitializationException(
+                    "Could not initialize data source backend", ex);
+        }
+    }
+
+    @Override
+    public Map<String, List<PrimitiveParameter>> getPrimitiveParametersAsc(
+            Set<String> keyIds, Set<String> paramIds,
+            Filter dataSourceConstraints, QuerySession qs)
+            throws DataSourceReadException {
+        boolean where = false;
+        final Set<String> keyIdsL = keyIds;
+        final Set<String> paramIdsL = paramIds;
+        final Long minValidL = null;
+        final Long maxValidL = null;
+        StringBuilder primParamAscStmt = new StringBuilder(
+                "SELECT keyId,paramId,value,valueType,hrsOffset FROM data WHERE paramId<>'ICD9' and paramId <>'ICD9DC'");
+        if (keyIds != null && !keyIds.isEmpty()) {
+            primParamAscStmt.append(" AND keyId IN (");
+            where = true;
+            primParamAscStmt.append(org.arp.javautil.collections.Collections.join(Collections.nCopies(keyIds.size(), "?"),
+                    ","));
+            primParamAscStmt.append(')');
+        }
+        if (paramIds != null && !paramIds.isEmpty()) {
+            if (where) {
+                primParamAscStmt.append(" AND paramId in (");
+            } else {
+                primParamAscStmt.append(" AND paramId IN (");
+                where = true;
+            }
+            primParamAscStmt.append(org.arp.javautil.collections.Collections.join(
+                    Collections.nCopies(paramIds.size(),
+                    "?"), ","));
+            primParamAscStmt.append(")");
+        }
+        if (minValidL != null) {
+            primParamAscStmt.append(" AND hrsOffset >= ?");
+        }
+        if (maxValidL != null) {
+            primParamAscStmt.append(" AND hrsOffset <= ?");
+        }
+        primParamAscStmt.append(" ORDER BY hrsOffset ASC");
+
+        StatementPreparer stmtPreparer = new StatementPreparer() {
+
+            @Override
+            public void prepare(PreparedStatement stmt)
+                    throws SQLException {
+                int pos = 1;
+                if (keyIdsL != null) {
+                    for (String keyId : keyIdsL) {
+                        stmt.setString(pos++, keyId);
+                    }
+                }
+                if (paramIdsL != null) {
+                    for (String paramId : paramIdsL) {
+                        stmt.setString(pos++, paramId);
+                    }
+                }
+                if (minValidL != null) {
+                    stmt.setLong(pos++, minValidL.longValue());
+                }
+                if (maxValidL != null) {
+                    stmt.setLong(pos++, maxValidL.longValue());
+                }
+            }
+        };
+
+        final Map<String, List<PrimitiveParameter>> result =
+                new HashMap<String, List<PrimitiveParameter>>();
+        ResultProcessor resultProcessor = new ResultProcessor() {
+
+            @Override
+            public void process(ResultSet resultSet) throws SQLException {
+                while (resultSet.next()) {
+                    String keyId = resultSet.getString(1);
+                    if (result.containsKey(keyId)) {
+                        PrimitiveParameter p = new PrimitiveParameter(
+                                resultSet.getString(2));
+                        p.setValue(ValueFormat.parse(
+                                resultSet.getString(3), 
+                                ValueType.valueOf(resultSet.getString(4))));
+                        p.setTimestamp(RelativeHourGranularity.HOUR
+                                .lengthInBaseUnit(resultSet.getInt(5)));
+                        p.setGranularity(RelativeHourGranularity.HOUR);
+                        List<PrimitiveParameter> l = result.get(keyId);
+                        l.add(p);
+                    } else {
+                        List<PrimitiveParameter> l =
+                                new ArrayList<PrimitiveParameter>();
+                        PrimitiveParameter p = new PrimitiveParameter(
+                                resultSet.getString(2));
+                        p.setValue(ValueFormat.parse(
+                                resultSet.getString(3), 
+                                ValueType.valueOf(resultSet.getString(4))));
+                        p.setTimestamp(RelativeHourGranularity.HOUR
+                                .lengthInBaseUnit(resultSet.getInt(5)));
+                        p.setGranularity(RelativeHourGranularity.HOUR);
+                        l.add(p);
+                        result.put(keyId, l);
+                    }
+
+                }
+            }
+        };
+        try {
+            SQLExecutor.executeSQL(this.connectionSpecInstance,
+                    primParamAscStmt.toString(), stmtPreparer, resultProcessor);
+        } catch (SQLException ex) {
+            throw new DataSourceReadException(ex);
+        }
+        return result;
+    }
+
+    @Override
+    public Map<String, List<ConstantParameter>> getConstantParameters(
+            Set<String> keyIds, Set<String> paramIds,
+            Filter dataSourceConstraints,
+            QuerySession qs)
+            throws DataSourceReadException {
+        return new HashMap<String, List<ConstantParameter>>(0);
+    }
+
+    @Override
+    public Map<String, List<Event>> getEventsAsc(Set<String> keyIds,
+            Set<String> paramIds, Filter filters, QuerySession qs)
+            throws DataSourceReadException {
+        boolean where = false;
+        final Set<String> keyIdsL = keyIds;
+        List<String> strippedParamIds = null;
+        if (paramIds != null) {
+            strippedParamIds = new ArrayList<String>(paramIds.size());
+            for (Iterator<String> itr = paramIds.iterator();
+                    itr.hasNext();) {
+                strippedParamIds.add(itr.next().substring(6));
+            }
+        }
+        final List<String> paramIdsL = strippedParamIds;
+        final Long minValidL = null;
+        final Long maxValidL = null;
+        StringBuilder primParamAscStmt = new StringBuilder(
+                "SELECT keyId,value,hrsOffset FROM data ");
+        if (keyIds != null && !keyIds.isEmpty()) {
+            primParamAscStmt.append(" WHERE paramId = 'ICD9' AND keyId IN (");
+            where = true;
+            primParamAscStmt.append(org.arp.javautil.collections.Collections.join(Collections.nCopies(keyIds.size(), "?"),
+                    ","));
+            primParamAscStmt.append(')');
+        }
+        if (paramIds != null && !paramIds.isEmpty()) {
+            if (where) {
+                primParamAscStmt.append(" AND value in (");
+            } else {
+                primParamAscStmt.append(" WHERE paramId = 'ICD9' AND value IN (");
+                where = true;
+            }
+            primParamAscStmt.append(org.arp.javautil.collections.Collections.join(
+                    Collections.nCopies(paramIds.size(),
+                    "?"), ","));
+            primParamAscStmt.append(")");
+        }
+        if (minValidL != null) {
+            primParamAscStmt.append(" AND hrsOffset >= ?");
+        }
+        if (maxValidL != null) {
+            primParamAscStmt.append(" AND hrsOffset <= ?");
+        }
+        primParamAscStmt.append(" ORDER BY hrsOffset ASC");
+
+        StatementPreparer stmtPreparer = new StatementPreparer() {
+
+            @Override
+            public void prepare(PreparedStatement stmt)
+                    throws SQLException {
+                int pos = 1;
+                if (keyIdsL != null) {
+                    for (Iterator<String> itr = keyIdsL.iterator(); itr.hasNext();) {
+                        stmt.setString(pos++, itr.next());
+                    }
+                }
+                if (paramIdsL != null) {
+                    for (Iterator<String> itr = paramIdsL.iterator(); itr.hasNext();) {
+                        stmt.setString(pos++, itr.next());
+                    }
+                }
+                if (minValidL != null) {
+                    stmt.setLong(pos++, minValidL.longValue());
+                }
+                if (maxValidL != null) {
+                    stmt.setLong(pos++, maxValidL.longValue());
+                }
+            }
+        };
+
+        final Map<String, List<Event>> result =
+                new HashMap<String, List<Event>>();
+        ResultProcessor resultProcessor = new ResultProcessor() {
+
+            @Override
+            public void process(ResultSet resultSet) throws SQLException {
+                while (resultSet.next()) {
+                    String keyId = resultSet.getString(1);
+                    if (result.containsKey(keyId)) {
+                        Event p = new Event("ICD-9_"
+                                + resultSet.getString(2));
+                        long l = RelativeHourGranularity.HOUR.lengthInBaseUnit(resultSet.getInt(3));
+                        p.setInterval(new PointInterval(l,
+                                RelativeHourGranularity.HOUR, l,
+                                RelativeHourGranularity.HOUR));
+                        List<Event> lll = result.get(keyId);
+                        lll.add(p);
+                    } else {
+                        List<Event> lll = new ArrayList<Event>();
+                        Event p = new Event("ICD-9_"
+                                + resultSet.getString(2));
+                        long l = RelativeHourGranularity.HOUR.lengthInBaseUnit(resultSet.getInt(3));
+                        p.setInterval(new PointInterval(l,
+                                RelativeHourGranularity.HOUR, l,
+                                RelativeHourGranularity.HOUR));
+                        lll.add(p);
+                        result.put(keyId, lll);
+                    }
+
+                }
+            }
+        };
+        try {
+            SQLExecutor.executeSQL(this.connectionSpecInstance, primParamAscStmt.toString(),
+                    stmtPreparer, resultProcessor);
+        } catch (SQLException ex) {
+            throw new DataSourceReadException(ex);
+        }
+        return result;
+    }
+
+    public boolean hasAttribute(String propId, String attributeId) {
+        return attributeId.equals("ATTR_VALUE");
+    }
+
+    @Override
+    public String getKeyType() {
+        return "CASE";
+    }
+
+    @Override
+    public String getKeyTypeDisplayName() {
+        return "case";
+    }
+
+    @Override
+    public String getKeyTypePluralDisplayName() {
+        return "cases";
+    }
 }
