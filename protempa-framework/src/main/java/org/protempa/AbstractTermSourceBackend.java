c3b99ec995767a7481ff5c7d95bd211beb01268c
Michel Mansour <michel.mansour@emory.edu>
2010-09-15 17:08:27 +0000

    support for term sources

---
+++ protempa-framework/src/main/java/org/protempa/AbstractTermSourceBackend.java
@@ -0,0 +1,46 @@
+package org.protempa;
+
+/**
+ * 
+ * @author Michel Mansour
+ */
+public abstract class AbstractTermSourceBackend extends
+        AbstractBackend<TermSourceBackendUpdatedEvent, TermSource> implements
+        TermSourceBackend {
+
+    /**
+     * A default implementation that returns null
+     * 
+     * @see org.protempa.TermSourceBackend#readTerm(java.lang.String,
+     * org.protempa.Terminology)
+     */
+    @Override
+    public Term readTerm(String id, Terminology terminology)
+            throws TermSourceReadException {
+        return null;
+    }
+
+    /**
+     * Implemented as a no-op
+     * 
+     * @see org.protempa.Backend#close()
+     */
+    @Override
+    public void close() {
+    }
+
+    /**
+     * A default implementation that returns null
+     * 
+     * @see org.protempa.Backend#getDisplayName()
+     */
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    protected void fireTermSourceBackendUpdated() {
+        fireBackendUpdated(new TermSourceBackendUpdatedEvent(this));
+    }
+
+}
