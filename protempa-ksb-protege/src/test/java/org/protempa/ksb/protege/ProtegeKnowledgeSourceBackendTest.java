89cf057cb0f953818e17ea476d158339bdc1a8f6
Michel Mansour <michel.mansour@emory.edu>
2010-11-18 21:13:07 +0000

    unit tests for ProtegeKnowledgeSourceBackend

---
+++ protempa-ksb-protege/src/test/java/org/protempa/ksb/protege/ProtegeKnowledgeSourceBackendTest.java
@@ -0,0 +1,79 @@
+package org.protempa.ksb.protege;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.protempa.backend.BackendPropertySpec;
+import org.protempa.backend.test.MockBackendInstanceSpec;
+import org.protempa.backend.test.MockBackendPropertySpec;
+
+public final class ProtegeKnowledgeSourceBackendTest {
+    private RemoteKnowledgeSourceBackend backend;
+
+    public ProtegeKnowledgeSourceBackendTest() {
+
+    }
+
+    @org.junit.BeforeClass
+    public static void setUpClass() throws Exception {
+
+    }
+
+    @org.junit.AfterClass
+    public static void tearDownClass() throws Exception {
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        this.backend = new RemoteKnowledgeSourceBackend();
+
+        backend.setHostname("aiwdev02.eushc.org");
+        backend.setUsername("mansour");
+        backend.setPassword("eHmMrKM2");
+        backend.setKnowledgeBaseName("ERATDiagnoses");
+        backend.setUnits("ABSOLUTE");
+
+        List<BackendPropertySpec> propSpecs = new ArrayList<BackendPropertySpec>();
+
+        propSpecs.add(new MockBackendPropertySpec("hostname", String.class)
+                .getBackendPropertySpec());
+        propSpecs.add(new MockBackendPropertySpec("username", String.class)
+                .getBackendPropertySpec());
+        propSpecs.add(new MockBackendPropertySpec("password", String.class)
+                .getBackendPropertySpec());
+        propSpecs.add(new MockBackendPropertySpec("knowledgeBaseName",
+                String.class).getBackendPropertySpec());
+        propSpecs.add(new MockBackendPropertySpec("units", String.class)
+                .getBackendPropertySpec());
+
+        MockBackendInstanceSpec<RemoteKnowledgeSourceBackend> mbis = new MockBackendInstanceSpec<RemoteKnowledgeSourceBackend>(
+                propSpecs);
+        
+        try {
+            backend.initialize(mbis.getBackendInstanceSpec());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("Setup complete!");
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        this.backend = null;
+    }
+    
+    @Test
+    public void testGetPropositionsByTerm() throws Exception {
+        
+    }
+    
+    @Test
+    public void testGetPropositionsByTermSubsumption() throws Exception {
+        
+    }
+}
