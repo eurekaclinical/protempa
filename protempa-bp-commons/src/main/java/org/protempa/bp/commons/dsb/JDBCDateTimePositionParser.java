fde4220c21be4cfa64910ec966fb61cb1aa52bb0
Andrew Post <arpost@emory.edu>
2010-12-07 23:41:40 +0000

    Renamed the JDBC datetime parser to a more obvious name.

---
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/JDBCDateTimePositionParser.java
@@ -0,0 +1,37 @@
+package org.protempa.bp.commons.dsb;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.sql.Types;
+
+/**
+ * Implements parsing of JDBC dates, times and timestamps.
+ * 
+ * @author Andrew Post
+ */
+public final class JDBCDateTimePositionParser implements PositionParser {
+
+    @Override
+    public long toLong(ResultSet resultSet, int columnIndex)
+            throws SQLException {
+        int colType = resultSet.getMetaData().getColumnType(columnIndex);
+        switch (colType) {
+            case Types.DATE:
+                Date date = resultSet.getDate(columnIndex);
+                return date.getTime();
+            case Types.TIME:
+                Time time = resultSet.getTime(columnIndex);
+                return time.getTime();
+            case Types.TIMESTAMP:
+                Timestamp timestamp = resultSet.getTimestamp(columnIndex);
+                return timestamp.getTime();
+            default:
+                throw new AssertionError("Not a date, time or timestamp!");
+        }
+
+    }
+
+}
