b25217cefc9deb6cd69bde8a88becf5cc58ae03c
Himanshu Rathod <himanshu.rathod@emory.edu>
2011-02-11 20:37:14 +0000

    Updated to use the new ResultCache class.

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/SQLGenerator.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/SQLGenerator.java
@@ -1,9 +1,11 @@
 package org.protempa.bp.commons.dsb;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
+
 import org.protempa.DataSourceReadException;
+import org.protempa.bp.commons.dsb.sqlgen.ConnectorJ5MySQL415Generator;
+import org.protempa.bp.commons.dsb.sqlgen.Ojdbc14Oracle10gSQLGenerator;
+import org.protempa.bp.commons.dsb.sqlgen.ResultCache;
 import org.protempa.bp.commons.dsb.sqlgen.SQLOrderBy;
 import org.protempa.dsb.filter.Filter;
 import org.protempa.proposition.Constant;
@@ -15,13 +17,13 @@ import org.protempa.proposition.value.UnitFactory;
 /**
  * An API for ServiceLoader-style services that generate database- and driver-
  * specific SQL for the {@link RelationalDatabaseSchemaAdaptor}.
- *
+ * 
  * The following database-specific SQL generators are provided:
  * <ul>
  * <li>{@link ConnectorJ5MySQL415Generator}
  * <li>{@link Ojdbc14Oracle10gSQLGenerator}
  * </ul>
- *
+ * 
  * The user may specify additional SQL generators by implementing this interface
  * and registering the implementation using the Java ServiceLoader method. Note
  * that if generators have overlapping compatibility, the generator that
@@ -31,19 +33,17 @@ import org.protempa.proposition.value.UnitFactory;
  */
 public interface SQLGenerator {
 
-    Map<String, List<Constant>> readConstants(
-            Set<String> keyIds, Set<String> paramIds,
-            Filter dataSourceConstraints)
+    ResultCache<Constant> readConstants(Set<String> keyIds,
+            Set<String> paramIds, Filter dataSourceConstraints)
             throws DataSourceReadException;
 
-    Map<String, List<PrimitiveParameter>> readPrimitiveParameters(
-            Set<String> keyIds, Set<String> paramIds,
+    ResultCache<PrimitiveParameter> readPrimitiveParameters(Set<String> keyIds,
+            Set<String> paramIds, Filter dataSourceConstraints, SQLOrderBy order)
+            throws DataSourceReadException;
+
+    ResultCache<Event> readEvents(Set<String> keyIds, Set<String> eventIds,
             Filter dataSourceConstraints, SQLOrderBy order)
             throws DataSourceReadException;
-
-    Map<String, List<Event>> readEvents(Set<String> keyIds,
-            Set<String> eventIds, Filter dataSourceConstraints,
-            SQLOrderBy order) throws DataSourceReadException;
 
     public GranularityFactory getGranularities();
 
