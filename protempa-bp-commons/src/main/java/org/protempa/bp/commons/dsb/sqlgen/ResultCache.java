843ad2e56c88bc211d9f70a7be0ae6acfc534975
Himanshu Rathod <himanshu.rathod@emory.edu>
2011-02-11 20:39:35 +0000

    Updated to use the transient keyword for the CachMap objects, to prevent a full cycle in the caching mechanism, caused by Location being an inner class.

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/ResultCache.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/ResultCache.java
@@ -1,36 +1,54 @@
 package org.protempa.bp.commons.dsb.sqlgen;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.arp.javautil.collections.Collections;
 import org.arp.javautil.map.CacheMap;
 import org.protempa.proposition.Proposition;
 import org.protempa.proposition.UniqueIdentifier;
 
-public class ResultCache<P extends Proposition> {
-    private final Map<String, List<P>> patientCache;
-    private final Map<UniqueIdentifier, P> propositionCache;
-    private final Map<UniqueIdentifier, Location> conversionMap;
+public class ResultCache<P extends Proposition> implements Serializable {
+    private static final long serialVersionUID = 7167245069602064844L;
+    transient private final Map<String, List<P>> patientCache;
+    transient private final Map<UniqueIdentifier, List<UniqueIdentifier>> referencesCache;
+    transient private final Map<UniqueIdentifier, Location> conversionMap;
 
     public ResultCache() {
         this.patientCache = new CacheMap<String, List<P>>();
-        this.propositionCache = new CacheMap<UniqueIdentifier, P>();
+        this.referencesCache = new CacheMap<UniqueIdentifier, List<UniqueIdentifier>>();
         this.conversionMap = new CacheMap<UniqueIdentifier, Location>();
     }
 
-    public void put(UniqueIdentifier uid, P prop) {
-        // first, we add to or update the propositions cache
-        this.propositionCache.put(uid, prop);
+    public List<P> getPatientPropositions(String patientKey) {
+        return this.patientCache.get(patientKey);
+    }
 
-        // now we update the patients cache using the conversion map, if
-        // we know where the proposition is located in the patient cache
-        if (this.conversionMap.containsKey(uid)) {
-            Location loc = this.conversionMap.get(uid);
-            List<P> propList = this.patientCache.get(loc
-                    .getPatientKey());
-            propList.set(loc.getIndex(), prop);
-            this.patientCache.put(loc.getPatientKey(), propList);
-        }
+    public P getProposition(UniqueIdentifier uid) {
+        Location loc = this.conversionMap.get(uid);
+        assert loc != null : "Could not find the location for proposition "
+                + uid;
+        return this.patientCache.get(loc.getPatientKey()).get(loc.getIndex());
+    }
+
+    public List<UniqueIdentifier> getReferences(UniqueIdentifier uid) {
+        return this.referencesCache.get(uid);
+    }
+
+    public Set<UniqueIdentifier> getReferenceKeys() {
+        return this.referencesCache.keySet();
+    }
+
+    public void addReference(UniqueIdentifier uid, UniqueIdentifier reference) {
+        // there should ever be a case where the proposition is
+        // added to the results BEFORE it's added as part of a patient's
+        // proposition list
+        assert this.conversionMap.containsKey(uid) : "Proposition being put is not contained in any patient's proposition list";
+
+        // now, we add to or update the propositions cache
+        Collections.putList(this.referencesCache, uid, reference);
     }
 
     public void put(String key, List<P> propList) {
@@ -43,9 +61,8 @@ public class ResultCache<P extends Proposition> {
         for (index = 0; index < propList.size(); index++) {
             P prop = propList.get(index);
             UniqueIdentifier uid = prop.getUniqueIdentifier();
-            this.propositionCache.put(uid, prop);
 
-            // we add a conversion from the UID to a location in the 
+            // we add a conversion from the UID to a location in the
             // patients cache if it doesn't already exist
             if (!this.conversionMap.containsKey(uid)) {
                 Location loc = new Location(key, index);
@@ -54,7 +71,12 @@ public class ResultCache<P extends Proposition> {
         }
     }
 
-    class Location {
+    public Map<String, List<P>> getPatientCache() {
+        return this.patientCache;
+    }
+
+    class Location implements Serializable {
+        private static final long serialVersionUID = 5829710457303104633L;
         private String patientKey;
         private int index;
 
