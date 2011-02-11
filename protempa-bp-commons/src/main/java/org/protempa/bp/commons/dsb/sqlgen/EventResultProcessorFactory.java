9919ff93235f181bd0864944e29b99e54c93bf60
Himanshu Rathod <himanshu.rathod@emory.edu>
2011-02-11 21:07:09 +0000

    Updated to use the new ResultCache class.

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/EventResultProcessorFactory.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/EventResultProcessorFactory.java
@@ -1,39 +1,29 @@
 package org.protempa.bp.commons.dsb.sqlgen;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.protempa.proposition.Event;
-import org.protempa.proposition.UniqueIdentifier;
 
 /**
- *
+ * 
  * @author Andrew Post
  */
-class EventResultProcessorFactory 
-        extends SQLGenResultProcessorFactory<Event> {
+class EventResultProcessorFactory extends SQLGenResultProcessorFactory<Event> {
 
     @Override
-    AbstractMainResultProcessor<Event> getInstance(
-            String dataSourceBackendId, EntitySpec entitySpec) {
-        Map<String, List<Event>> results =
-                new HashMap<String, List<Event>>();
+    AbstractMainResultProcessor<Event> getInstance(String dataSourceBackendId,
+            EntitySpec entitySpec, ResultCache<Event> cache) {
 
-        EventResultProcessor resultProcessor =
-                new EventResultProcessor();
-        resultProcessor.setResults(results);
+        EventResultProcessor resultProcessor = new EventResultProcessor();
+        resultProcessor.setResults(cache);
         resultProcessor.setDataSourceBackendId(dataSourceBackendId);
         resultProcessor.setEntitySpec(entitySpec);
         return resultProcessor;
     }
 
     @Override
-    RefResultProcessor<Event> getRefInstance(
-            String dataSourceBackendId, EntitySpec entitySpec,
-            ReferenceSpec referenceSpec,
-            Map<UniqueIdentifier,Event> cache) {
-        EventRefResultProcessor resultProcessor =
-                new EventRefResultProcessor();
+    RefResultProcessor<Event> getRefInstance(String dataSourceBackendId,
+            EntitySpec entitySpec, ReferenceSpec referenceSpec,
+            ResultCache<Event> cache) {
+        EventRefResultProcessor resultProcessor = new EventRefResultProcessor();
         resultProcessor.setCache(cache);
         resultProcessor.setReferenceSpec(referenceSpec);
         resultProcessor.setDataSourceBackendId(dataSourceBackendId);
