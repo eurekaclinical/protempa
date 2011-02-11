29bdddec69a83e6cade9325d879237b78ee623b0
Himanshu Rathod <himanshu.rathod@emory.edu>
2011-02-11 21:05:23 +0000

    Updated to use the new ResultCache class.

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/SQLGenResultProcessorFactory.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/SQLGenResultProcessorFactory.java
@@ -1,20 +1,17 @@
 package org.protempa.bp.commons.dsb.sqlgen;
 
-import java.util.Map;
 import org.protempa.proposition.Proposition;
-import org.protempa.proposition.UniqueIdentifier;
 
 /**
- *
+ * 
  * @author Andrew Post
  */
 abstract class SQLGenResultProcessorFactory<P extends Proposition> {
     abstract AbstractMainResultProcessor<P> getInstance(
-            String dataSourceBackendId,
-            EntitySpec entitySpec);
+            String dataSourceBackendId, EntitySpec entitySpec,
+            ResultCache<P> cache);
 
-    abstract RefResultProcessor<P> getRefInstance(
-            String dataSourceBackendId,
+    abstract RefResultProcessor<P> getRefInstance(String dataSourceBackendId,
             EntitySpec entitySpec, ReferenceSpec referenceSpec,
-            Map<UniqueIdentifier,P> cache);
+            ResultCache<P> cache);
 }
