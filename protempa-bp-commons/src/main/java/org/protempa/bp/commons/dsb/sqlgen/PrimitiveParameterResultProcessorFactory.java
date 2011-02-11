b2c5c2e6b677760ab98edb0be8a9e24bfdedf182
Himanshu Rathod <himanshu.rathod@emory.edu>
2011-02-11 21:05:54 +0000

    Updated to use the new ResultCache class.

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/PrimitiveParameterResultProcessorFactory.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/sqlgen/PrimitiveParameterResultProcessorFactory.java
@@ -1,27 +1,21 @@
 package org.protempa.bp.commons.dsb.sqlgen;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.protempa.proposition.PrimitiveParameter;
-import org.protempa.proposition.UniqueIdentifier;
 
 /**
- *
+ * 
  * @author Andrew Post
  */
-class PrimitiveParameterResultProcessorFactory 
-        extends SQLGenResultProcessorFactory<PrimitiveParameter> {
+class PrimitiveParameterResultProcessorFactory extends
+        SQLGenResultProcessorFactory<PrimitiveParameter> {
 
     @Override
     AbstractMainResultProcessor<PrimitiveParameter> getInstance(
-            String dataSourceBackendId, EntitySpec entitySpec) {
-        Map<String, List<PrimitiveParameter>> results =
-                new HashMap<String, List<PrimitiveParameter>>();
+            String dataSourceBackendId, EntitySpec entitySpec,
+            ResultCache<PrimitiveParameter> cache) {
 
-        PrimitiveParameterResultProcessor resultProcessor =
-                new PrimitiveParameterResultProcessor();
-        resultProcessor.setResults(results);
+        PrimitiveParameterResultProcessor resultProcessor = new PrimitiveParameterResultProcessor();
+        resultProcessor.setResults(cache);
         resultProcessor.setDataSourceBackendId(dataSourceBackendId);
         resultProcessor.setEntitySpec(entitySpec);
         return resultProcessor;
@@ -30,10 +24,8 @@ class PrimitiveParameterResultProcessorFactory
     @Override
     RefResultProcessor<PrimitiveParameter> getRefInstance(
             String dataSourceBackendId, EntitySpec entitySpec,
-            ReferenceSpec referenceSpec,
-            Map<UniqueIdentifier,PrimitiveParameter> cache) {
-        PrimitiveParameterRefResultProcessor resultProcessor =
-                new PrimitiveParameterRefResultProcessor();
+            ReferenceSpec referenceSpec, ResultCache<PrimitiveParameter> cache) {
+        PrimitiveParameterRefResultProcessor resultProcessor = new PrimitiveParameterRefResultProcessor();
         resultProcessor.setCache(cache);
         resultProcessor.setReferenceSpec(referenceSpec);
         resultProcessor.setDataSourceBackendId(dataSourceBackendId);
