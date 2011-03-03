ea9b516b7f5494a07c6b34da6fd02be350190a93
Andrew Post <arpost@emory.edu>
2011-03-03 16:34:15 +0000

    Fixed NullPointerException. Just pull date and time always, since Oracle apparently allows DATE columns to have a time.

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/JDBCDateTimePositionParser.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/JDBCDateTimePositionParser.java
@@ -1,11 +1,9 @@
 package org.protempa.bp.commons.dsb;
 
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
-import java.sql.Timestamp;
 import java.sql.Types;
+import java.util.Date;
 
 /**
  * Implements parsing of JDBC dates, times and timestamps.
@@ -15,22 +13,14 @@ import java.sql.Types;
 public final class JDBCDateTimePositionParser implements PositionParser {
 
     @Override
-    public long toLong(ResultSet resultSet, int columnIndex, int colType)
+    public Long toLong(ResultSet resultSet, int columnIndex, int colType)
             throws SQLException {
-        switch (colType) {
-            case Types.DATE:
-                Date date = resultSet.getDate(columnIndex);
-                return date.getTime();
-            case Types.TIME:
-                Time time = resultSet.getTime(columnIndex);
-                return time.getTime();
-            case Types.TIMESTAMP:
-                Timestamp timestamp = resultSet.getTimestamp(columnIndex);
-                return timestamp.getTime();
-            default:
-                throw new AssertionError("Not a date, time or timestamp!");
+        Date result = resultSet.getTimestamp(columnIndex);
+        if (result != null) {
+            return result.getTime();
+        } else {
+            return null;
         }
 
     }
-
 }
