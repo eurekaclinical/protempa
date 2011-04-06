ee99ece1e45e3df0dcf1a63349ba1eeea10272a7
Andrew Post <arpost@emory.edu>
2011-04-06 16:07:38 +0000

    Added documentation, and changed exceptions to throw the same exceptions as java.util.ServiceLoader.

--- javautil/src/main/java/org/arp/javautil/serviceloader/ServiceLoader.java
+++ javautil/src/main/java/org/arp/javautil/serviceloader/ServiceLoader.java
@@ -8,53 +8,90 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.ServiceConfigurationError;
 
 /**
- *
+ * Functions like {@link java.util.ServiceLoader} except it returns
+ * lists of {@link Class}es instead of an {@link java.lang.Iterable} of
+ * instances.
+ * 
  * @author Andrew Post
  */
 public class ServiceLoader {
 
-    public static <S> List<Class<? extends S>> load(Class<S> ifc)
-            throws IOException, ClassNotFoundException {
-        return load(ifc, null);
+    /**
+     * Loads the classes of the providers of a service.
+     *
+     * @param service a service type.
+     * @return a list of classes implementing the given service type, using
+     * the current thread's context class loader.
+     *
+     * @throws ServiceConfigurationError if something goes wrong with loading
+     * a service provider's class.
+     */
+    public static <S> List<Class<? extends S>> load(Class<S> service) {
+        return load(service, null);
     }
 
-    public static <S> List<Class<? extends S>> load(Class<S> ifc,
-            ClassLoader ldr)
-            throws IOException, ClassNotFoundException  {
-        if (ldr == null)
-            ldr = Thread.currentThread().getContextClassLoader();
-        Enumeration<URL> e = ldr.getResources("META-INF/services/" +
-                ifc.getName());
+    /**
+     * Loads the classes of the providers of a service.
+     *
+     * @param service a service type. If <code>null</code>, a
+     * {@link NullPointerException} will be thrown.
+     * @param loader a class loader. If <code>null</code>, the current thread's
+     * context class loader will be used.
+     * @return a list of classes implementing the given service type and
+     * class loader.
+     *
+     * @throws ServiceConfigurationError if something goes wrong with loading
+     * a service provider's class.
+     */
+    public static <S> List<Class<? extends S>> load(Class<S> service,
+            ClassLoader loader) {
+        if (loader == null) {
+            loader = Thread.currentThread().getContextClassLoader();
+        }
         List<Class<? extends S>> services = new ArrayList<Class<? extends S>>();
-        while (e.hasMoreElements()) {
-            URL url = e.nextElement();
-            InputStream is = url.openStream();
-            try {
-                BufferedReader r = new BufferedReader(
-                        new InputStreamReader(is, "UTF-8"));
-                while (true) {
-                    String line = r.readLine();
-                    if (line == null) {
-                        break;
+        try {
+            Enumeration<URL> e = loader.getResources("META-INF/services/"
+                    + service.getName());
+
+            while (e.hasMoreElements()) {
+                URL url = e.nextElement();
+                InputStream is = url.openStream();
+                String name = null;
+                try {
+                    BufferedReader r = new BufferedReader(
+                            new InputStreamReader(is, "UTF-8"));
+                    while (true) {
+                        String line = r.readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        int comment = line.indexOf('#');
+                        if (comment >= 0) {
+                            line = line.substring(0, comment);
+                        }
+                        name = line.trim();
+                        if (name.length() == 0) {
+                            continue;
+                        }
+                        Class<?> clz = Class.forName(name, true, loader);
+                        Class<? extends S> impl = clz.asSubclass(service);
+
+                        services.add(impl);
                     }
-                    int comment = line.indexOf('#');
-                    if (comment >= 0) {
-                        line = line.substring(0, comment);
-                    }
-                    String name = line.trim();
-                    if (name.length() == 0) {
-                        continue;
-                    }
-                    Class<?> clz = Class.forName(name, true, ldr);
-                    Class<? extends S> impl = clz.asSubclass(ifc);
-                    
-                    services.add(impl);
+                } catch (ClassNotFoundException cnfe) {
+                    throw new ServiceConfigurationError("Could not load class for service provider " + name, cnfe);
+                } finally {
+                    is.close();
                 }
-            } finally {
-                is.close();
             }
+        } catch (IOException ioe) {
+            /**
+             * {@link java.util.ServiceLoader} swallows this exception, so
+             * will we.
+             */
         }
         return services;
     }
