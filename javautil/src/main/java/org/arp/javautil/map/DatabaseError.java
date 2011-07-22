7154846de1416d3889f611e3c0ca6ad1d6f1c18c
Michel Mansour <michel.mansour@emory.edu>
2011-07-22 14:19:56 +0000

    Deprecated and replaced by the new data store classes and interfaces. Moved to new org.arp.javautil.datastore package. See PROTEMPA-64 in JIRA.

--- javautil/src/main/java/org/arp/javautil/map/DatabaseError.java
+++
@@ -1,9 +0,0 @@
-package org.arp.javautil.map;
-
-public class DatabaseError extends RuntimeException {
-    private static final long serialVersionUID = 3861144884838037475L;
-
-    DatabaseError(Throwable t) {
-        super(t);
-    }
-}
