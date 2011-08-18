d6d40894989392c420abe05cd4111f6c7cc0d2e5
Michel Mansour <michel.mansour@emory.edu>
2011-08-18 20:11:51 +0000

    added class javadoc removed unnecessary "public" qualifier from shutdown() method

--- javautil/src/main/java/org/arp/javautil/datastore/DataStore.java
+++ javautil/src/main/java/org/arp/javautil/datastore/DataStore.java
@@ -2,10 +2,22 @@ package org.arp.javautil.datastore;
 
 import java.util.Map;
 
+/**
+ * Represents a data store, which can be either temporary or persistent. It can
+ * be treated just like a {@link java.util.Map}, but defines one extra method,
+ * <code>shutdown</code>.
+ * 
+ * @param <K>
+ *            the key type to store
+ * @param <V>
+ *            the value type to store
+ * 
+ * @author Michel Mansour
+ */
 public interface DataStore<K, V> extends Map<K, V> {
 
     /**
      * Performs any clean up of the store and shuts it down.
      */
-    public void shutdown();
+    void shutdown();
 }
