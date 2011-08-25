43d2e2cdd240ec7c05296b874fb7e94f2e347e61
Michel Mansour <michel.mansour@emory.edu>
2011-08-25 21:00:35 +0000

    added documentation and convenience methods for staged execution process

--- protempa-framework/src/main/java/org/protempa/Protempa.java
+++ protempa-framework/src/main/java/org/protempa/Protempa.java
@@ -20,8 +20,7 @@ import org.protempa.query.handler.QueryResultsHandler;
  * @author Andrew Post
  */
 public final class Protempa {
-    private static final String STARTUP_FAILURE_MSG = 
-            "PROTEMPA could not start up";
+    private static final String STARTUP_FAILURE_MSG = "PROTEMPA could not start up";
     private final AbstractionFinder abstractionFinder;
 
     public static Protempa newInstance(String configurationId)
@@ -253,7 +252,76 @@ public final class Protempa {
         logger.info("Query execution complete");
     }
 
-    public void retrieveDataAndPersist(Query query, String persistentStoreName)
+    /**
+     * Executes all three phases of the PROTEMPA life cycle (data retrieval,
+     * processing, and output) while also persisting intermediate results,
+     * specifically the results of the proposition retrieval and processing.
+     * 
+     * @param query
+     *            the query to execute
+     * @param resultHandler
+     *            a result handler specifying how the output should be produced
+     * @param retrievalStoreName
+     *            the name of the persistent store that will hold the
+     *            propositions retrieved from the data source
+     * @param processStoreName
+     *            the name of the persistent store that will hold the results of
+     *            PROTEMPA processing the retrieved propositions
+     * @throws FinderException
+     *             if PROTEMPA fails to complete for any reason
+     */
+    public void executeWithPersistence(Query query,
+            QueryResultsHandler resultHandler, String retrievalStoreName,
+            String processStoreName) throws FinderException {
+        if (query == null) {
+            throw new IllegalArgumentException("query cannot be null");
+        }
+        if (query.getTermIds().length > 0) {
+            throw new UnsupportedOperationException(
+                    "term id support has not been implemented yet.");
+        }
+        Logger logger = ProtempaUtil.logger();
+        logger.log(
+                Level.INFO,
+                "Executing all PROTEMPA phases. Will store retrieved propositions in {0} and derived propositions in {1}",
+                new String[] { retrievalStoreName, processStoreName });
+
+        Set<String> keyIds = Arrays.asSet(query.getKeyIds());
+        Set<String> propIds = Arrays.asSet(query.getPropIds());
+        Set<And<String>> termIds = Arrays.asSet(query.getTermIds());
+        Filter filters = query.getFilters();
+        QuerySession qs = new QuerySession(query, this.abstractionFinder);
+
+        logger.log(Level.INFO, "Beginning data retrieval stage");
+        this.abstractionFinder.retrieveData(keyIds, propIds, termIds, filters,
+                qs, retrievalStoreName);
+        logger.log(Level.INFO, "Data retrieval complete");
+        logger.log(Level.INFO, "Beginning processing stage");
+        this.abstractionFinder.processStoredResults(keyIds, propIds, qs,
+                retrievalStoreName, processStoreName);
+        logger.log(Level.INFO, "Processing complete");
+        logger.log(Level.INFO, "Beginning output stage");
+        this.abstractionFinder.outputStoredResults(keyIds, propIds,
+                resultHandler, qs, processStoreName);
+        logger.log(Level.INFO, "Output complete");
+    }
+
+    /**
+     * Executes the first stage in PROTEMPA's life cycle. Executes a PROTEMPA
+     * query in the data source and persistently stores the resulting
+     * propositions.
+     * 
+     * @param query
+     *            the query to execute in the data source
+     * @param retrievalStoreName
+     *            the name of the persistent store for holding the propositions.
+     *            This name should be provided to subsequent calls to
+     *            {@link #processResultsAndPersist(Query, String, String)} as
+     *            the name of the proposition data store.
+     * @throws FinderException
+     *             if the retrieval could not complete
+     */
+    public void retrieveDataAndPersist(Query query, String retrievalStoreName)
             throws FinderException {
         if (query == null) {
             throw new IllegalArgumentException("query cannot be null");
@@ -266,20 +334,42 @@ public final class Protempa {
         logger.log(Level.FINE, "Retrieving and persisting data");
         logger.log(Level.INFO,
                 "Retrieved data will be persisted in store named: {0}",
-                persistentStoreName);
+                retrievalStoreName);
         Set<String> keyIdsSet = Arrays.asSet(query.getKeyIds());
         Set<String> propIds = Arrays.asSet(query.getPropIds());
         Set<And<String>> termIds = Arrays.asSet(query.getTermIds());
         Filter filters = query.getFilters();
         QuerySession qs = new QuerySession(query, this.abstractionFinder);
         this.abstractionFinder.retrieveData(keyIdsSet, propIds, termIds,
-                filters, qs, persistentStoreName);
+                filters, qs, retrievalStoreName);
         logger.log(Level.FINE, "Data retrieval complete");
     }
 
-    public void processResultsAndPersist(Query query,
-            String propositionStoreName, String workingMemoryStoreName)
-            throws FinderException {
+    /**
+     * Executes the second stage in PROTEMPA's life cycle. Processes the results
+     * of a data retrieval with respect the given query. The results of the
+     * processing are persisted for later use.
+     * 
+     * @param query
+     *            the original query used for data retrieval
+     * @param keyIds
+     *            if not empty or null, the processing will be restricted to the
+     *            key IDs in this set
+     * @param propositionIds
+     *            the proposition IDs with respect to which processing should be
+     *            done
+     * @param retrievalStoreName
+     *            the name of the persistent store containing the propositions
+     *            to process; should be the same name as the one provided to
+     *            {@link #retrieveDataAndPersist(Query, String)}.
+     * @param workingMemoryStoreName
+     *            the name of the persistent store to use the processed data
+     * @throws FinderException
+     *             if processing fails to complete
+     */
+    public void processResultsAndPersist(Query query, Set<String> keyIds,
+            Set<String> propositionIds, String retrievalStoreName,
+            String workingMemoryStoreName) throws FinderException {
         if (query == null) {
             throw new IllegalArgumentException("query cannot be null");
         }
@@ -294,24 +384,90 @@ public final class Protempa {
                 workingMemoryStoreName);
         logger.log(Level.INFO,
                 "Pulling previously retrieved data from store named: {0}",
-                propositionStoreName);
-        Set<String> propIds = Arrays.asSet(query.getPropIds());
+                retrievalStoreName);
         QuerySession qs = new QuerySession(query, this.abstractionFinder);
 
-        this.abstractionFinder.processStoredResults(propIds, qs,
-                propositionStoreName, workingMemoryStoreName);
+        this.abstractionFinder.processStoredResults(keyIds, propositionIds, qs,
+                retrievalStoreName, workingMemoryStoreName);
         logger.log(Level.FINE, "Data processing complete");
     }
 
-    public void outputResults(Query query, QueryResultsHandler resultHandler,
-            String workingMemoryStoreName) throws FinderException {
-        outputResults(query, Arrays.asSet(query.getPropIds()), resultHandler,
+    /**
+     * Identical to
+     * {@link #processResultsAndPersist(Query, Set, Set, String, String)} except
+     * uses the key IDs and propositions defined in the query for processing.
+     * 
+     * @param query
+     *            the original query used to retrieve the data
+     * @param propositionStoreName
+     *            name of the persistent store holding the propositions
+     * @param workingMemoryStoreName
+     *            name of the persistent store intended to hold the processed
+     *            data
+     * @throws FinderException
+     *             if processing fails to complete
+     */
+    public void processResultsAndPersist(Query query,
+            String propositionStoreName, String workingMemoryStoreName)
+            throws FinderException {
+        processResultsAndPersist(query, Arrays.asSet(query.getKeyIds()),
+                Arrays.asSet(query.getPropIds()), propositionStoreName,
                 workingMemoryStoreName);
     }
 
-    public void outputResults(Query query, Set<String> propositionIds,
-            QueryResultsHandler resultHandler, String workingMemoryStoreName)
-            throws FinderException {
+    /**
+     * The third and final stage in PROTEMPA's lifecylce. Identical to
+     * {@link #outputResults(Query, Set, Set, QueryResultsHandler, String)}
+     * except the proposition IDs are simply taken from the query. Outputs
+     * previously processed results, with respect to the given query. The query
+     * does not have to be the same as those in either of the previous two
+     * stages.
+     * 
+     * @param query
+     *            the original query used to retrieve the data
+     * @param resultHandler
+     *            a result handler that specifies how the output should be
+     *            produced
+     * @param workingMemoryStoreName
+     *            the name of the persistent store containing the processed
+     *            propositions; should be the same as the one provided to
+     *            {@link #processResultsAndPersist}.
+     * @throws FinderException
+     *             if the output fails to complete
+     */
+    public void outputResults(Query query, QueryResultsHandler resultHandler,
+            String workingMemoryStoreName) throws FinderException {
+        outputResults(query, Arrays.asSet(query.getKeyIds()),
+                Arrays.asSet(query.getPropIds()), resultHandler,
+                workingMemoryStoreName);
+    }
+
+    /**
+     * The third and final stage in PROTEMPA's lifecylce. Outputs previously
+     * processed results, with respect to the given proposition IDs. The query
+     * does not have to be the same as those in either of the previous two
+     * stages.
+     * 
+     * @param query
+     *            the original query used to retrieve the data
+     * @param keyIds
+     *            if not empty or null, the output will be restricted to the key
+     *            IDs in this set
+     * @param propositionIds
+     *            the propositions in the processed data to output
+     * @param resultHandler
+     *            a result handler that specifies how the output should be
+     *            produced
+     * @param workingMemoryStoreName
+     *            the name of the persistent store containing the processed
+     *            propositions; should be the same as the one provided to
+     *            {@link #processResultsAndPersist(Query, Set, Set, String, String)}
+     * @throws FinderException
+     *             if the output fails to complete
+     */
+    public void outputResults(Query query, Set<String> keyIds,
+            Set<String> propositionIds, QueryResultsHandler resultHandler,
+            String workingMemoryStoreName) throws FinderException {
         if (query == null) {
             throw new IllegalArgumentException("query cannot be null");
         }
@@ -325,16 +481,35 @@ public final class Protempa {
                 "Retrieving processed results from store named: {0}",
                 workingMemoryStoreName);
         QuerySession qs = new QuerySession(query, this.abstractionFinder);
-        this.abstractionFinder.outputStoredResults(propositionIds,
+        this.abstractionFinder.outputStoredResults(keyIds, propositionIds,
                 resultHandler, qs, workingMemoryStoreName);
         logger.log(Level.FINE, "Output complete");
     }
 
+    /**
+     * Performs the second and third stages (process and output) of PROTEMPA's
+     * life cycle without storing the processed results.
+     * 
+     * @param origQuery
+     *            the query originally used to query the data
+     * @param newQuery
+     *            UNUSED
+     * @param resultHandler
+     *            a result handler that specifies how the output should be
+     *            produced
+     * @param propositionStoreName
+     *            the name of the persistent store containing the retrieved
+     *            propositions; should be the same as the one provided to
+     *            {@link #retrieveDataAndPersist(Query, String)}
+     * @throws FinderException
+     *             if the processing and output fail to complete
+     */
     public void processResultsAndOutput(Query origQuery, Query newQuery,
             QueryResultsHandler resultHandler, String propositionStoreName)
             throws FinderException {
         QuerySession qs = new QuerySession(origQuery, this.abstractionFinder);
         this.abstractionFinder.processAndOutputStoredResults(
+                Arrays.asSet(origQuery.getKeyIds()),
                 Arrays.asSet(origQuery.getPropIds()), resultHandler, qs,
                 propositionStoreName);
     }
