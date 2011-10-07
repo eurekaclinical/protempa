7192b8e6df682d117ed65750723ca670754387a3
Michel Mansour <michel.mansour@emory.edu>
2011-10-07 16:07:51 +0000

    removed individual clause accessors that were only added for testing convenience

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/relationaldb/SelectStatement.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/relationaldb/SelectStatement.java
@@ -1,21 +1,11 @@
 package org.protempa.bp.commons.dsb.relationaldb;
 
-import java.util.List;
-import java.util.Set;
+/**
+ * A select statement to be executed.
+ * 
+ * @author Michel Mansour
+ *
+ */
+public interface SelectStatement extends SqlStatement {
 
-import org.protempa.backend.dsb.filter.Filter;
-
-interface SelectStatement extends SqlStatement {
-    // TODO: remove these before committing
-    
-    SelectClause getSelectClause(ColumnSpecInfo info,
-            TableAliaser referenceIndices, EntitySpec entitySpec);
-
-    FromClause getFromClause(List<ColumnSpec> columnSpecs,
-            TableAliaser referenceIndices);
-
-    WhereClause getWhereClause(Set<String> propIds, ColumnSpecInfo info,
-            List<EntitySpec> entitySpecs, Set<Filter> filters,
-            TableAliaser referenceIndices, Set<String> keyIds,
-            SQLOrderBy order, SQLGenResultProcessor resultProcessor, SelectClause selectClause);
 }
