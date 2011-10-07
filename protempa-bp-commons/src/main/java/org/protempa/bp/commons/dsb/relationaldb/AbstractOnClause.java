d917d45ae9c6058cb4b5666d252f2b7979bb7e75
Michel Mansour <michel.mansour@emory.edu>
2011-10-07 15:38:01 +0000

    removed some obsolete lines

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/relationaldb/AbstractOnClause.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/relationaldb/AbstractOnClause.java
@@ -5,29 +5,21 @@ abstract class AbstractOnClause implements OnClause {
     private final JoinSpec joinSpec;
     private final TableAliaser referenceIndices;
 
-    AbstractOnClause(JoinSpec joinSpec,
-            TableAliaser referenceIndices) {
+    AbstractOnClause(JoinSpec joinSpec, TableAliaser referenceIndices) {
         this.joinSpec = joinSpec;
         this.referenceIndices = referenceIndices;
     }
 
     @Override
     public String generateClause() {
-        // return new StringBuilder("on (")
-        // .append(SqlGeneratorUtil.generateColumnReference(stmt, fromIndex,
-        // fromKey))
-        // .append(" = ")
-        // .append(SqlGeneratorUtil.generateColumnReference(stmt, toIndex,
-        // toKey))
-        // .append(") ").toString();
-
         return new StringBuilder("on (")
-                .append(referenceIndices.generateTableReference(joinSpec.getPrevColumnSpec()))
+                .append(referenceIndices.generateTableReference(joinSpec
+                        .getPrevColumnSpec()))
                 .append("." + joinSpec.getFromKey())
                 .append(" = ")
-                .append(referenceIndices.generateTableReference(joinSpec.getNextColumnSpec()))
-                .append("." + joinSpec.getToKey())
-                .append(") ").toString();
+                .append(referenceIndices.generateTableReference(joinSpec
+                        .getNextColumnSpec()))
+                .append("." + joinSpec.getToKey()).append(") ").toString();
     }
 
 }
