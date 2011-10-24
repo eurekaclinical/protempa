f0e0a181ef22735860312c9841249f25e63021b6
Mark Grand <mgrand@emory.edu>
2011-10-24 16:21:14 +0000

    Added serialVersionUID

--- protempa-framework/src/main/java/org/protempa/backend/BackendInitializationException.java
+++ protempa-framework/src/main/java/org/protempa/backend/BackendInitializationException.java
@@ -7,7 +7,12 @@ import org.protempa.ProtempaException;
  * @author Andrew Post
  */
 public abstract class BackendInitializationException extends ProtempaException {
-    protected BackendInitializationException(String reason) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 656273236938235529L;
+
+	protected BackendInitializationException(String reason) {
         super(reason);
     }
 
