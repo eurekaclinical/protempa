3d5ce0de774cd82101001316e76bf64690b01144
Michel Mansour <michel.mansour@emory.edu>
2011-11-03 21:11:24 +0000

    changed the javadoc on isClosed to be closer to English

--- javautil/src/main/java/org/arp/javautil/datastore/DataStore.java
+++ javautil/src/main/java/org/arp/javautil/datastore/DataStore.java
@@ -4,8 +4,8 @@ import java.util.Map;
 
 /**
  * Represents a data store, which can be either temporary or persistent. It can
- * be treated just like a {@link java.util.Map}, but defines one extra method,
- * <code>shutdown</code>.
+ * be treated just like a {@link java.util.Map}, but defines two extra methods,
+ * <code>shutdown</code>, which closes the store, and <code>isClosed</code>..
  * 
  * @param <K>
  *            the key type to store
@@ -22,7 +22,7 @@ public interface DataStore<K, V> extends Map<K, V> {
     void shutdown();
 
     /**
-     * Checks whether the store has already been shutdown
+     * Checks whether the store has already been shut down
      * 
      * @return <code>true</code> if the store has been shutdown;
      *         <code>false</code> otherwise
