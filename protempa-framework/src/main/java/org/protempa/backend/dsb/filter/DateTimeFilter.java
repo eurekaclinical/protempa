ca3c589e429dc4a2b65b848e08ee055e279fdd60
Mark Grand <mgrand@emory.edu>
2011-11-09 12:53:32 +0000

    Added a constructor for Castor that takes Granularity and Side as strings.

--- protempa-framework/src/main/java/org/protempa/backend/dsb/filter/DateTimeFilter.java
+++ protempa-framework/src/main/java/org/protempa/backend/dsb/filter/DateTimeFilter.java
@@ -53,4 +53,32 @@ public class DateTimeFilter extends PositionFilter {
                 finish != null ? finish.getTime() : null, finishGran,
                 startSide, finishSide);
     }
+
+    /**
+     * Creates a filter with a date/time range. This constructor allows granularity 
+     * and side to be specified as strings.  This constructor is intended for use 
+     * by Castor. 
+     *
+     * @param propIds a {@link String[]} of proposition ids on which to filter.
+     * @param start the start {@link Date} (<code>null</code> for unspecified).
+     * @param startGran the name of the {@link Granularity} with which to interpret the
+     * start date.
+     * @param finish the finish {@link Date} (<code>null</code> for
+     * unspecified).
+     * @param finishGran the anme of the {@link Granularity with which to interpret the
+     * finish date.
+     * @param startSide the name of the {@link Side} of the proposition to which to apply
+     * the start bound. The default is {@link Side.START} (if
+     * <code>null</code> is specified).
+     * @param finishSide the name of the {@link Side} of the proposition to which to
+     * apply the finish bound. The default is {@link Side.FINISH} (if
+     * <code>null</code> is specified).
+     */
+    public DateTimeFilter(String[] propIds, Date start,
+            String startGran, Date finish, String finishGran,
+            String startSide, String finishSide) {
+        super(propIds, start != null ? start.getTime() : null, startGran,
+                finish != null ? finish.getTime() : null, finishGran,
+                startSide, finishSide);
+    }
 }
