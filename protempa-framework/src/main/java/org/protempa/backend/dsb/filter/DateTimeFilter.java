b8c54df454f94c1cbe6352b0eb03aaae6d5718b9
Mark Grand <mgrand@emory.edu>
2011-11-11 13:38:10 +0000

    Organized setters and constructors for Castor.

--- protempa-framework/src/main/java/org/protempa/backend/dsb/filter/DateTimeFilter.java
+++ protempa-framework/src/main/java/org/protempa/backend/dsb/filter/DateTimeFilter.java
@@ -10,25 +10,27 @@ import org.protempa.proposition.value.Granularity;
  */
 public class DateTimeFilter extends PositionFilter {
 
-    /**
-     * Creates a filter with a date/time range.
-     *
-     * @param propIds a {@link String[]} of proposition ids on which to filter.
-     * @param start the start {@link Date} (<code>null</code> for unspecified).
-     * @param startGran the {@link Granularity} with which to interpret the
-     * start date.
-     * @param finish the finish {@link Date} (<code>null</code> for
-     * unspecified).
-     * @param finishGran the {@link Granularity with which to interpret the
-     * finish date.
-     */
-    public DateTimeFilter(String[] propIds, Date start,
-            Granularity startGran, Date finish, Granularity finishGran) {
-        super(propIds, start != null ? start.getTime() : null, startGran,
-                finish != null ? finish.getTime() : null, finishGran);
-    }
+	/**
+	 * Creates a filter with a date/time range.
+	 * 
+	 * @param propIds
+	 *            a {@link String[]} of proposition ids on which to filter.
+	 * @param start
+	 *            the start {@link Date} (<code>null</code> for unspecified).
+	 * @param startGran
+	 *            the {@link Granularity} with which to interpret the start
+	 *            date.
+	 * @param finish
+	 *            the finish {@link Date} (<code>null</code> for unspecified).
+	 * @param finishGran
+	 *            the {@link Granularity with which to interpret the finish
+	 *            date.
+	 */
+	public DateTimeFilter(String[] propIds, Date start, Granularity startGran, Date finish, Granularity finishGran) {
+		super(propIds, start != null ? start.getTime() : null, startGran, finish != null ? finish.getTime() : null, finishGran);
+	}
 
-    /**
+/**
      * Creates a filter with a date/time range.
      *
      * @param propIds a {@link String[]} of proposition ids on which to filter.
@@ -46,39 +48,39 @@ public class DateTimeFilter extends PositionFilter {
      * apply the finish bound. The default is {@link Side.FINISH} (if
      * <code>null</code> is specified).
      */
-    public DateTimeFilter(String[] propIds, Date start,
-            Granularity startGran, Date finish, Granularity finishGran,
-            Side startSide, Side finishSide) {
-        super(propIds, start != null ? start.getTime() : null, startGran,
-                finish != null ? finish.getTime() : null, finishGran,
-                startSide, finishSide);
-    }
+	public DateTimeFilter(String[] propIds, Date start, Granularity startGran, Date finish, Granularity finishGran, Side startSide, Side finishSide) {
+		super(propIds, start != null ? start.getTime() : null, startGran, finish != null ? finish.getTime() : null, finishGran, startSide, finishSide);
+	}
 
-    /**
-     * Creates a filter with a date/time range. This constructor allows granularity 
-     * and side to be specified as strings.  This constructor is intended for use 
-     * by Castor. 
-     *
-     * @param propIds a {@link String[]} of proposition ids on which to filter.
-     * @param start the start {@link Date} (<code>null</code> for unspecified).
-     * @param startGran the name of the {@link Granularity} with which to interpret the
-     * start date.
-     * @param finish the finish {@link Date} (<code>null</code> for
-     * unspecified).
-     * @param finishGran the anme of the {@link Granularity with which to interpret the
-     * finish date.
-     * @param startSide the name of the {@link Side} of the proposition to which to apply
-     * the start bound. The default is {@link Side.START} (if
-     * <code>null</code> is specified).
-     * @param finishSide the name of the {@link Side} of the proposition to which to
-     * apply the finish bound. The default is {@link Side.FINISH} (if
-     * <code>null</code> is specified).
-     */
-    public DateTimeFilter(String[] propIds, Date start,
-            String startGran, Date finish, String finishGran,
-            String startSide, String finishSide) {
-        super(propIds, start != null ? start.getTime() : null, startGran,
-                finish != null ? finish.getTime() : null, finishGran,
-                startSide, finishSide);
-    }
+	/**
+	 * @return the interval start as a date.
+	 */
+	public Date getStartDate() {
+		return new Date(getStart());
+	}
+
+	/**
+	 * Setter for Castor.
+	 * 
+	 * @param dt
+	 */
+	public void setStartDate(Date dt) {
+		setStart(dt.getTime());
+	}
+
+	/**
+	 * @return the interval finish as a date.
+	 */
+	public Date getFinishDate() {
+		return new Date(getFinish());
+	}
+
+	/**
+	 * Setter for Castor.
+	 * 
+	 * @param dt
+	 */
+	public void setFinishDate(Date dt) {
+		setFinish(dt.getTime());
+	}
 }
--- protempa-framework/src/main/java/org/protempa/backend/dsb/filter/PositionFilter.java
+++ protempa-framework/src/main/java/org/protempa/backend/dsb/filter/PositionFilter.java
@@ -8,88 +8,116 @@ import org.protempa.proposition.value.XMLGranularityFactory;
 
 /**
  * A filter for a position (e.g., date/time) range.
- *
+ * 
  * @author Andrew Post
  */
 public class PositionFilter extends AbstractFilter {
 
-    /**
-     * Which side of the proposition's interval to which to apply the position
-     * bounds. For timestamped propositions, these are ignored.
-     */
-    public static enum Side {
-        START("start"), FINISH("finish");
-        
-        private String xmlName;
-        
-        private Side(String xmlName) {
-        	this.xmlName = xmlName;
-        }
-        
-        public String getXmlName() {
-        	return xmlName;
-        }
-    }
+	/**
+	 * Which side of the proposition's interval to which to apply the position
+	 * bounds. For timestamped propositions, these are ignored.
+	 */
+	public static enum Side {
+		START("start"), FINISH("finish");
 
-    private static final IntervalFactory intervalFactory =
-            new IntervalFactory();
+		private String xmlName;
 
-    private final Interval ival;
+		private Side(String xmlName) {
+			this.xmlName = xmlName;
+		}
 
-    private final Side startSide;
+		public String getXmlName() {
+			return xmlName;
+		}
+	}
 
-    private final Side finishSide;
+	private static final IntervalFactory intervalFactory = new IntervalFactory();
 
-    /**
-     * Creates a filter with a position range.
-     *
-     * @param propIds a {@link String[]} of proposition ids on which to filter.
-     * @param start the start position in Protempa's {@link Long}
-     * representation.
-     * @param startGran the {@link Granularity} with which to interpret the
-     * start position.
-     * @param finish the finish position in Protempa's {@link Long}
-     * representation.
-     * @param finishGran the {@link Granularity} with which to interpret the
-     * finish position.
-     */
-    public PositionFilter(String[] propIds, Long start,
-            Granularity startGran, Long finish, Granularity finishGran) {
-        this(propIds, start, startGran, finish, finishGran, null, null);
-    }
+	private Long start;
+	private Granularity startGran;
+	private Long finish;
+	Granularity finishGran;
 
-    /**
-     * Initializes a filter with a position range.
-     *
-     * @param propIds a {@link String[]} of proposition ids on which to filter.
-     * @param start the start position in Protempa's {@link Long}
-     * representation.
-     * @param startGran the {@link Granularity} with which to interpret the
-     * start position.
-     * @param finish the finish position in Protempa's {@link Long}
-     * representation.
-     * @param finishGran the {@link Granularity} with which to interpret the
-     * finish position.
-     * @param startSide the {@link Side} of the proposition to which to apply
-     * the start bound. The default is {@link Side.START} (if
-     * <code>null</code> is specified). Ignored for timestamped propositions.
-     * @param finishSide the {@link Side} of the proposition to which to
-     * apply the finish bound. The default is {@link Side.FINISH} (if
-     * <code>null</code> is specified). Ignored for timestamped propositions.
-     */
-    public PositionFilter(String[] propIds, Long start,
-            Granularity startGran, Long finish, Granularity finishGran,
-            Side startSide, Side finishSide) {
-        super(propIds);
-        this.ival = intervalFactory.getInstance(start, startGran,
-                finish, finishGran);
-        if (startSide == null)
-            startSide = Side.START;
-        if (finishSide == null)
-            finishSide = Side.FINISH;
-        this.startSide = startSide;
-        this.finishSide = finishSide;
-    }
+	private Interval ival;
+
+	private Side startSide;
+
+	private Side finishSide;
+
+	public PositionFilter() {
+
+	}
+
+	/**
+	 * Creates a filter with a position range.
+	 * 
+	 * @param propIds
+	 *            a {@link String[]} of proposition ids on which to filter.
+	 * @param start
+	 *            the start position in Protempa's {@link Long} representation.
+	 * @param startGran
+	 *            the {@link Granularity} with which to interpret the start
+	 *            position.
+	 * @param finish
+	 *            the finish position in Protempa's {@link Long} representation.
+	 * @param finishGran
+	 *            the {@link Granularity} with which to interpret the finish
+	 *            position.
+	 */
+	public PositionFilter(String[] propIds, Long start, Granularity startGran, Long finish, Granularity finishGran) {
+		this(propIds, start, startGran, finish, finishGran, null, null);
+	}
+
+	/**
+	 * Initializes a filter with a position range.
+	 * 
+	 * @param propIds
+	 *            a {@link String[]} of proposition ids on which to filter.
+	 * @param start
+	 *            the start position in Protempa's {@link Long} representation.
+	 * @param startGran
+	 *            the {@link Granularity} with which to interpret the start
+	 *            position.
+	 * @param finish
+	 *            the finish position in Protempa's {@link Long} representation.
+	 * @param finishGran
+	 *            the {@link Granularity} with which to interpret the finish
+	 *            position.
+	 * @param startSide
+	 *            the {@link Side} of the proposition to which to apply the
+	 *            start bound. The default is {@link Side.START} (if
+	 *            <code>null</code> is specified). Ignored for timestamped
+	 *            propositions.
+	 * @param finishSide
+	 *            the {@link Side} of the proposition to which to apply the
+	 *            finish bound. The default is {@link Side.FINISH} (if
+	 *            <code>null</code> is specified). Ignored for timestamped
+	 *            propositions.
+	 */
+	public PositionFilter(String[] propIds, Long start, Granularity startGran, Long finish, Granularity finishGran, Side startSide, Side finishSide) {
+		super(propIds);
+		this.start = start;
+		this.startGran = startGran;
+		this.finish = finish;
+		this.finishGran = finishGran;
+		this.startSide = startSide;
+		this.finishSide = finishSide;
+		init();
+	}
+
+	/**
+	 * Complete this object's initialization. This method should be called
+	 * externally only by Castor.
+	 */
+	public void init() {
+		if (ival == null) {
+			this.ival = intervalFactory.getInstance(start, startGran, finish, finishGran);
+			if (startSide == null)
+				startSide = Side.START;
+			if (finishSide == null)
+				finishSide = Side.FINISH;
+		}
+	}
 
 	/**
 	 * Initializes a filter with a position range.
@@ -120,174 +148,199 @@ public class PositionFilter extends AbstractFilter {
 	 *            <code>null</code> is specified). Ignored for timestamped
 	 *            propositions.
 	 */
-    public PositionFilter(String[] propIds, Long start,
-            String startGran, Long finish, String finishGran,
-            String startSide, String finishSide) {
-        this(propIds, start, nameToGranularity(startGran), finish, nameToGranularity(finishGran), nameToSide(startSide), nameToSide(finishSide));
-    }
-    
-    private static Side nameToSide(String name) {
-    	if (Side.START.getXmlName().equals(name)) {
-    		return Side.START;
-    	}
-    	if (Side.FINISH.getXmlName().equals(name)) {
-    		return Side.FINISH;
-    	} else {
-    		return null;
-    	}
-    }
-    
-    private static Granularity nameToGranularity(String name) {
-    	Granularity g = XMLGranularityFactory.xmlToGranularity(name);
-    	if (g == null) {
-    		String msg = "Value passed to constructor of " + PositionFilter.class + " is not a valid name for a granularity";
-    		throw new IllegalArgumentException(msg);
-    	}
-    	return g;
-    }
+	public PositionFilter(String[] propIds, Long start, String startGran, Long finish, String finishGran, String startSide, String finishSide) {
+		this(propIds, start, nameToGranularity(startGran), finish, nameToGranularity(finishGran), nameToSide(startSide), nameToSide(finishSide));
+	}
 
-    /**
-     * Returns the {@link Granularity} with which to interpret the
-     * start position.
-     * 
-     * @return the startGranularity a {@link Granularity}.
-     */
-    public Granularity getStartGranularity() {
-        return this.ival.getStartGranularity();
-    }
+	private static Side nameToSide(String name) {
+		if (Side.START.getXmlName().equals(name)) {
+			return Side.START;
+		}
+		if (Side.FINISH.getXmlName().equals(name)) {
+			return Side.FINISH;
+		} else {
+			return null;
+		}
+	}
 
-    /**
-     * Returns the XML name of the {@link Granularity} with which to interpret the
-     * start position.
-     * 
-     * @return the startGranularity a {@link Granularity}.
-     */
-    public String getStartGranularityXMLName() {
-        return XMLGranularityFactory.granularityToXml(this.ival.getStartGranularity());
-    }
+	private static Granularity nameToGranularity(String name) {
+		Granularity g = XMLGranularityFactory.xmlToGranularity(name);
+		if (g == null) {
+			String msg = name + " is not a valid name for a granularity";
+			throw new IllegalArgumentException(msg);
+		}
+		return g;
+	}
 
-    /**
-     * Returns the {@link Granularity} with which to interpret the
-     * finish position.
-     *
-     * @return the finishGranularity a {@link Granularity}.
-     */
-    public Granularity getFinishGranularity() {
-        return this.ival.getFinishGranularity();
-    }
+	/**
+	 * Returns the {@link Granularity} with which to interpret the start
+	 * position.
+	 * 
+	 * @return the startGranularity a {@link Granularity}.
+	 */
+	public Granularity getStartGranularity() {
+		return startGran;
+	}
 
-    /**
-     * Returns the name of the {@link Granularity} with which to interpret the
-     * finish position.
-     *
-     * @return the finishGranularity a {@link Granularity}.
-     */
-    public String getFinishGranularityXMLName() {
-        return XMLGranularityFactory.granularityToXml(this.ival.getFinishGranularity());
-    }
+	/**
+	 * Returns the XML name of the {@link Granularity} with which to interpret
+	 * the start position.
+	 * 
+	 * @return the startGranularity a {@link Granularity}.
+	 */
+	public String getStartGranularityXMLName() {
+		return XMLGranularityFactory.granularityToXml(this.ival.getStartGranularity());
+	}
 
-    /**
-     * Returns the maximum finish position in Protempa's {@link Long}
-     * representation.
-     *
-     * @return a {@link Long}.
-     */
-    public Long getMaximumFinish() {
-        return this.ival.getMaximumFinish();
-    }
+	/**
+	 * Setter for Castor.
+	 * 
+	 * @param name
+	 */
+	void setStartGranularityXMLName(String name) {
+		startGran = nameToGranularity(name);
+	}
 
-    /**
-     * Returns the maximum start position in Protempa's {@link Long}
-     * representation.
-     *
-     * @return a {@link Long}.
-     */
-    public Long getMaximumStart() {
-        return this.ival.getMaximumStart();
-    }
+	/**
+	 * Returns the {@link Granularity} with which to interpret the finish
+	 * position.
+	 * 
+	 * @return the finishGranularity a {@link Granularity}.
+	 */
+	public Granularity getFinishGranularity() {
+		return finishGran;
+	}
 
-    /**
-     * Returns the minimum finish position in Protempa's {@link Long}
-     * representation.
-     *
-     * @return a {@link Long}.
-     */
-    public Long getMinimumFinish() {
-        return this.ival.getMinimumFinish();
-    }
+	/**
+	 * Returns the name of the {@link Granularity} with which to interpret the
+	 * finish position.
+	 * 
+	 * @return the finishGranularity a {@link Granularity}.
+	 */
+	public String getFinishGranularityXMLName() {
+		return XMLGranularityFactory.granularityToXml(this.ival.getFinishGranularity());
+	}
 
-    /**
-     * Returns the minimum start position in Protempa's {@link Long}
-     * representation.
-     *
-     * @return a {@link Long}.
-     */
-    public Long getMinimumStart() {
-        return this.ival.getMinimumStart();
-    }
+	void setFinishGranularityXMLName(String name) {
+		finishGran = nameToGranularity(name);
+	}
 
-    /**
-     * Returns the start position supplied in the constructor.
-     *
-     * @return a {@link Long}.
-     */
-    public Long getStart() {
-        return this.ival.getMinStart();
-    }
+	/**
+	 * Returns the maximum finish position in Protempa's {@link Long}
+	 * representation.
+	 * 
+	 * @return a {@link Long}.
+	 */
+	public Long getMaximumFinish() {
+		return this.ival.getMaximumFinish();
+	}
 
-    /**
-     * Returns the finish position supplied in the constructor.
-     *
-     * @return a {@link Long}.
-     */
-    public Long getFinish() {
-        return this.ival.getMinFinish();
-    }
+	/**
+	 * Returns the maximum start position in Protempa's {@link Long}
+	 * representation.
+	 * 
+	 * @return a {@link Long}.
+	 */
+	public Long getMaximumStart() {
+		return this.ival.getMaximumStart();
+	}
 
-    /**
-     * Returns the side of the proposition to which to apply the lower bound.
-     *
-     * @return a {@link Side}.
-     */
-    public Side getStartSide() {
-        return this.startSide;
-    }
+	/**
+	 * Returns the minimum finish position in Protempa's {@link Long}
+	 * representation.
+	 * 
+	 * @return a {@link Long}.
+	 */
+	public Long getMinimumFinish() {
+		return this.ival.getMinimumFinish();
+	}
 
-    /**
-     * Returns the name of the side of the proposition to which to apply the lower bound.
-     *
-     * @return a {@link Side}.
-     */
-    public String getStartSideXMLName() {
-        return this.startSide.getXmlName();
-    }
+	/**
+	 * Returns the minimum start position in Protempa's {@link Long}
+	 * representation.
+	 * 
+	 * @return a {@link Long}.
+	 */
+	public Long getMinimumStart() {
+		return this.ival.getMinimumStart();
+	}
 
-    /**
-     * Returns the side of the proposition to which to apply the upper bound.
-     *
-     * @return a {@link Side}.
-     */
-    public Side getFinishSide() {
-        return this.finishSide;
-    }
+	/**
+	 * Returns the start position supplied in the constructor.
+	 * 
+	 * @return a {@link Long}.
+	 */
+	public Long getStart() {
+		return start;
+	}
 
-    /**
-     * Returns the name of the side of the proposition to which to apply the upper bound.
-     *
-     * @return a {@link Side}.
-     */
-    public String getFinishSideXMLName() {
-        return this.finishSide.getXmlName();
-    }
+	/**
+	 * Setter for Castor.
+	 * 
+	 * @param start
+	 */
+	void setStart(long start) {
+		this.start = start;
+	}
 
-    @Override
-    public void accept(FilterVisitor visitor) {
-        visitor.visit(this);
-    }
+	/**
+	 * Returns the finish position supplied in the constructor.
+	 * 
+	 * @return a {@link Long}.
+	 */
+	public Long getFinish() {
+		return finish;
+	}
 
+	void setFinish(long finish) {
+		this.finish = finish;
+	}
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
-    }
+	/**
+	 * Returns the side of the proposition to which to apply the lower bound.
+	 * 
+	 * @return a {@link Side}.
+	 */
+	public Side getStartSide() {
+		return this.startSide;
+	}
+
+	/**
+	 * Returns the name of the side of the proposition to which to apply the
+	 * lower bound.
+	 * 
+	 * @return a {@link Side}.
+	 */
+	public String getStartSideXMLName() {
+		return this.startSide.getXmlName();
+	}
+
+	/**
+	 * Returns the side of the proposition to which to apply the upper bound.
+	 * 
+	 * @return a {@link Side}.
+	 */
+	public Side getFinishSide() {
+		return this.finishSide;
+	}
+
+	/**
+	 * Returns the name of the side of the proposition to which to apply the
+	 * upper bound.
+	 * 
+	 * @return a {@link Side}.
+	 */
+	public String getFinishSideXMLName() {
+		return this.finishSide.getXmlName();
+	}
+
+	@Override
+	public void accept(FilterVisitor visitor) {
+		visitor.visit(this);
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 }
