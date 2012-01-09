dd7fe5c701dba6b1c5a4299cf08881cd29312144
Michel Mansour <michel.mansour@emory.edu>
2012-01-09 22:47:32 +0000

    SelectClause implementation specific to staging. It ignores all of the ColumnSpecs in the EntitySpec, using only the ColumnSpecs defined in the StagingSpec

---
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/relationaldb/StagingSelectClause.java
@@ -0,0 +1,41 @@
+package org.protempa.bp.commons.dsb.relationaldb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.protempa.bp.commons.dsb.relationaldb.ColumnSpec.KnowledgeSourceIdToSqlCode;
+
+final class StagingSelectClause implements SelectClause {
+
+    private final StagingSpec stagingSpec;
+    private final TableAliaser referenceIndices;
+    
+    public StagingSelectClause(StagingSpec stagingSpec, TableAliaser referenceIndices) {
+        this.stagingSpec = stagingSpec;
+        this.referenceIndices = referenceIndices;
+    }
+    
+    @Override
+    public String generateClause() {
+        StringBuilder result = new StringBuilder();
+        
+        result.append("SELECT ");
+        
+        List<String> cols = new ArrayList<String>();
+        for (ColumnSpec columnSpec : this.stagingSpec.getStagedColumns()) {
+            cols.add(referenceIndices.generateColumnReference(columnSpec));
+        }
+        result.append(StringUtils.join(cols, ','));
+                
+        return result.toString();
+    }
+
+    @Override
+    public void setCaseClause(Object[] sqlCodes, TableAliaser referenceIndices,
+            ColumnSpec columnSpec,
+            KnowledgeSourceIdToSqlCode[] filteredConstraintValues) {
+        throw new UnsupportedOperationException();
+    }
+
+}
