5d17f0a0aa1ec4549a9a3ebe60c990f1d9cf72cf
Michel Mansour <michel.mansour@emory.edu>
2012-01-11 22:33:36 +0000

    modified method definitions to conform to new APIs

--- protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/relationaldb/WhereClause.java
+++ protempa-bp-commons/src/main/java/org/protempa/bp/commons/dsb/relationaldb/WhereClause.java
@@ -2,10 +2,8 @@ package org.protempa.bp.commons.dsb.relationaldb;
 
 
 interface WhereClause extends SqlClause {
-    InClause getInClause(ColumnSpec columnSpec, Object[] elements, boolean not,
-            TableAliaser referenceIndices);
+    InClause getInClause(ColumnSpec columnSpec, Object[] elements, boolean not);
 
     OrderByClause getOrderByClause(ColumnSpec startColumnSpec,
-            ColumnSpec finishColumnSpec, SQLOrderBy order,
-            TableAliaser referenceIndices);
+            ColumnSpec finishColumnSpec);
 }
