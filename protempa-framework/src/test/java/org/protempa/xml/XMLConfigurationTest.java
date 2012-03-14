dc1eeb8a28f2d27a91b60ede8b5ba41714aea70f
Michel Mansour <michel.mansour@emory.edu>
2012-03-14 16:44:03 +0000

    fixed a typo in the noNamespaceSchemaLocation URL that was causing the test to fail

--- protempa-framework/src/test/java/org/protempa/xml/XMLConfigurationTest.java
+++ protempa-framework/src/test/java/org/protempa/xml/XMLConfigurationTest.java
@@ -111,8 +111,8 @@ public class XMLConfigurationTest extends TestCase {
 		XMLConfiguration.writeQueryAsXML(reconstitutedQuery, z2);
 		FileReader freader = new FileReader(z2);
 		String xml = new BufferedReader(freader).readLine();
-		assertTrue(xml.contains("xsi:noNamespaceSchemaLocation=\"http://aiwdev02.eusch.org/protempa/schema/1.1/protempa_query.xsd\""));
-		
+		System.out.println(xml);
+		assertTrue(xml.contains("xsi:noNamespaceSchemaLocation=\"http://aiwdev02.eushc.org/protempa/schema/1.1/protempa_query.xsd\""));
 	}
 
 	private void checkXMLValid(File file, String xsd) throws SAXException, ParserConfigurationException, IOException {
