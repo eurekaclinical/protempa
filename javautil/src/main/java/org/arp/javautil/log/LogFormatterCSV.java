25a2e33f446e08f00b4d3511c22fcd2431f9a414
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-03-23 18:58:24 +0000

    Removed unnecessary imports.

--- javautil/src/main/java/org/arp/javautil/log/LogFormatterCSV.java
+++ javautil/src/main/java/org/arp/javautil/log/LogFormatterCSV.java
@@ -19,18 +19,6 @@
  */
 package org.arp.javautil.log;
 
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-
-
 public class LogFormatterCSV extends LogFormatterSingleLine {
     
     public LogFormatterCSV(){
