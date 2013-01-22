76a2b39f3245bf47df67b93e5edba0c8bb558d09
Andrew Post <arpost@emory.edu>
2013-01-22 01:38:16 +0000

    Removed stale TODO.

--- protempa-framework/src/main/java/org/protempa/xml/PropertyConstraintConverter.java
+++ protempa-framework/src/main/java/org/protempa/xml/PropertyConstraintConverter.java
@@ -93,7 +93,6 @@ class PropertyConstraintConverter extends AbstractConverter {
 			expectNoMore(reader);
 		}
 
-		// TODO Auto-generated method stub
 		return new PropertyConstraint(propertyName, comparator, value);
 	}
 
