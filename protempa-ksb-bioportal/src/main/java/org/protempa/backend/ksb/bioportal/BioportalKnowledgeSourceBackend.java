3d6deeacca7c83f3d664be30bbae2fd71ce8bbad
Michel Mansour <michel.mansour@emory.edu>
2014-03-04 16:49:25 -0500

    changed to support pulling propositions from an h2 database

--- protempa-ksb-bioportal/src/main/java/org/protempa/backend/ksb/bioportal/BioportalKnowledgeSourceBackend.java
+++ protempa-ksb-bioportal/src/main/java/org/protempa/backend/ksb/bioportal/BioportalKnowledgeSourceBackend.java
@@ -1,14 +1,40 @@
 package org.protempa.backend.ksb.bioportal;
 
+/*
+ * #%L
+ * Protempa BioPortal Knowledge Source Backend
+ * %%
+ * Copyright (C) 2012 - 2014 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.protempa.AbstractionDefinition;
 import org.protempa.ContextDefinition;
 import org.protempa.KnowledgeSourceReadException;
 import org.protempa.PropositionDefinition;
 import org.protempa.TemporalPropositionDefinition;
 import org.protempa.backend.AbstractCommonsKnowledgeSourceBackend;
+import org.protempa.backend.BackendInitializationException;
+import org.protempa.backend.BackendInstanceSpec;
+import org.protempa.backend.KnowledgeSourceBackendInitializationException;
 import org.protempa.backend.annotations.BackendInfo;
 import org.protempa.backend.annotations.BackendProperty;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -17,25 +43,30 @@ import java.util.List;
 @BackendInfo(displayName = "BioPortal Knowledge Source Backend")
 public class BioportalKnowledgeSourceBackend extends AbstractCommonsKnowledgeSourceBackend {
 
-    private String apiKey;
-    private String ontologyUrl;
+    /* the ID of the database that holds the ontologies */
+    private String databaseId;
 
-    public String getApiKey() {
-        return apiKey;
+    private Connection conn;
+
+    public String getDatabaseId() {
+        return databaseId;
     }
 
-    @BackendProperty(displayName = "API Key")
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
+    @BackendProperty
+    public void setDatabaseId(String databaseId) {
+        this.databaseId = databaseId;
     }
 
-    public String getOntologyUrl() {
-        return ontologyUrl;
-    }
-
-    @BackendProperty(displayName = "Ontology URL")
-    public void setOntologyUrl(String ontologyUrl) {
-        this.ontologyUrl = ontologyUrl;
+    @Override
+    public void initialize(BackendInstanceSpec config) throws BackendInitializationException {
+        super.initialize(config);
+        if (this.conn == null) {
+            try {
+                this.conn = DriverManager.getConnection(this.databaseId);
+            } catch (SQLException e) {
+                throw new KnowledgeSourceBackendInitializationException("Failed to initialize BioPortal knowledge source backend", e);
+            }
+        }
     }
 
     @Override
