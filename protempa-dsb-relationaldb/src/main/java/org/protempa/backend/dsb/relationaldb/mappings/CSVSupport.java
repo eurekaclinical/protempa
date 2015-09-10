1e6074387e7483073c9192c210ed835bc483f1a2
Andrew Post <arpost@emory.edu>
2015-09-10 17:17:13 -0400

    Don't trim the columns because some codes may actually have leading or trailing whitespace (like MIMIC-II).

--- protempa-dsb-relationaldb/src/main/java/org/protempa/backend/dsb/relationaldb/mappings/CSVSupport.java
+++ protempa-dsb-relationaldb/src/main/java/org/protempa/backend/dsb/relationaldb/mappings/CSVSupport.java
@@ -52,13 +52,13 @@ public class CSVSupport {
                     case 0:
                         break;
                     case 1:
-                        String col0 = cols[0].trim();
+                        String col0 = cols[0];
                         if (col0.length() > 1) {
                             cvs.put(col0, "");
                         }
                         break;
                     case 2:
-                        cvs.put(cols[1].trim(), cols[0].trim());
+                        cvs.put(cols[1], cols[0]);
                         break;
                     default:
                         throw new AssertionError("Invalid mapping in line " + i + ": mapping has length " + cols.length);
