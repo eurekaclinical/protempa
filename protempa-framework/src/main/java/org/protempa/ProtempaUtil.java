3414a8971e7a6aa4eb2d6cb9fb37b1b39ce860ad
Andrew Post <arpost@emory.edu>
2018-08-01 10:52:49 -0400

    Clean up import statements.

--- protempa-framework/src/main/java/org/protempa/ProtempaUtil.java
+++ protempa-framework/src/main/java/org/protempa/ProtempaUtil.java
@@ -21,13 +21,9 @@ package org.protempa;
 
 import java.text.MessageFormat;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 import org.protempa.proposition.TemporalProposition;
 import org.protempa.proposition.comparator.TemporalPropositionIntervalComparator;
